a12d32cc15b44c2fb55aee712f45ad665d0ff085
Giacomo Vianello <giacomov@slac.stanford.edu>
2015-11-30 15:18:13 -0800

    Code cleanup and reformatting

--- astromodels/Parameter.py
+++ astromodels/Parameter.py
@@ -1,12 +1,15 @@
 __author__ = 'giacomov'
 
+import warnings
+import exceptions
+
+
 def _behaves_like_a_number(obj):
-    '''
+    """
 
     :param obj:
     :return: True if obj supports addition, subtraction, multiplication and division. False otherwise.
-    '''
-
+    """
 
     try:
 
@@ -23,9 +26,14 @@ def _behaves_like_a_number(obj):
 
         return True
 
-class Parameter(object):
 
-    '''
+# Read-only exception
+class SettingOutOfBounds(exceptions.Exception):
+    pass
+
+
+class Parameter(object):
+    """
 
     Implements a numerical parameter.
 
@@ -108,26 +116,27 @@ class Parameter(object):
 
     More than one callback can be registered. The callbacks will be executed in the order they are entered. To clear all
     callbacks use the method empty_callbacks().
-    '''
+    """
 
     def __init__(self, name, initial_value, **kwargs):
 
-        #NOTE: we avoid enforcing a particular type or even that initial_values, max, min and delta are python numbers.
-        #Indeed, as long as they behave as numbers we are going to be fine. We want to keep the possibility to use
-        #numbers coming from C, C++ or other sources, for example. Note however that string parameters are not supported
+        # NOTE: we avoid enforcing a particular type or even that initial_values, max, min and delta are python numbers.
+        # Indeed, as long as they behave as numbers we are going to be fine. We want to keep the possibility to use
+        # numbers coming from C, C++ or other sources, for example. Note however that string
+        # parameters are not supported
 
-        #Callbacks are executed any time the value for the parameter changes (i.e., its value changes)
+        # Callbacks are executed any time the value for the parameter changes (i.e., its value changes)
 
-        #We start from a empty list of callbacks.
+        # We start from a empty list of callbacks.
         self._callbacks = []
 
-        #Assign to members
+        # Assign to members
 
-        self.name = name
+        self.__name = name
 
         self._value = initial_value
 
-        #Set minimum if provided, otherwise use default
+        # Set minimum if provided, otherwise use default
 
         if 'min' in kwargs.keys():
 
@@ -137,7 +146,7 @@ class Parameter(object):
 
             self._min_value = None
 
-        #Set maximum if provided, otherwise use default
+        # Set maximum if provided, otherwise use default
 
         if 'max' in kwargs.keys():
 
@@ -147,7 +156,7 @@ class Parameter(object):
 
             self._max_value = None
 
-        #Set delta if provided, otherwise use default
+        # Set delta if provided, otherwise use default
 
         if 'delta' in kwargs.keys():
 
@@ -157,166 +166,186 @@ class Parameter(object):
 
             self._delta = 0.1 * self._value
 
-        #pre-defined prior is no prior
+        # pre-defined prior is no prior
         self._prior = None
 
-        #Now perform a very lazy check that we can perform math operations on all members
+        # Now perform a very lazy check that we can perform math operations on all members
 
         if not _behaves_like_a_number(self._value):
-
             raise TypeError("The provided initial value is not a number")
 
         if not _behaves_like_a_number(self._delta):
-
             raise TypeError("The provided delta is not a number")
 
         if self._min_value is not None:
 
             if not _behaves_like_a_number(self._min_value):
-
                 raise TypeError("The provided minimum value is not a number")
 
         if self._max_value is not None:
 
             if not _behaves_like_a_number(self._max_value):
-
                 raise TypeError("The provided maximum value is not a number")
 
+    # Define the property "name" but make it read-only
+
+    @property
+    def name(self):
+        return self.__name
+
+    # Define the property "value" with a control that the parameter cannot be set
+    # outside of its bounds
+
     def get_value(self):
-        '''Return current parameter value'''
+        """Return current parameter value"""
         return self._value
 
     def set_value(self, value):
         """Sets the current value of the parameter, ensuring that it is within the allowed range"""
 
         if self._min_value is not None and value < self._min_value:
-
-            raise ValueError(
+            raise SettingOutOfBounds(
                 "Trying to set parameter {0} = {1}, which is less than the minimum allowed {2}".format(
                     self.name, value, self._min_value))
 
         if self._max_value is not None and value > self._max_value:
-
-            raise ValueError(
+            raise SettingOutOfBounds(
                 "Trying to set parameter {0} = {1}, which is more than the maximum allowed {2}".format(
                     self.name, value, self._max_value))
 
-        #Call the callbacks (if any)
+        # Call the callbacks (if any)
 
         for callback in self._callbacks:
 
             try:
 
-                callback( value )
+                callback(value)
 
             except:
 
-                raise RuntimeError("Could not get callback for parameter %s with value %s" %(self.name, value))
+                raise RuntimeError("Could not get callback for parameter %s with value %s" % (self.name, value))
 
         self._value = value
 
     value = property(get_value, set_value,
-                         doc="""Gets or sets the value for the parameter""")
+                     doc="""Gets or sets the value for the parameter""")
+
+    # Define the property "delta"
 
     def get_delta(self):
-        '''Gets the current value for the delta of the parameter'''
+        """Gets the current value for the delta of the parameter"""
         return self._delta
 
     def set_delta(self, delta):
-        '''Sets the current delta for the parameter. The delta is used as initial step by some fitting engines'''
+        """Sets the current delta for the parameter. The delta is used as initial step by some fitting engines"""
 
         if not _behaves_like_a_number(delta):
-
             raise ValueError("Provided delta is not a number" % delta)
 
         self._delta = delta
 
-    delta = property(get_delta,set_delta,
+    delta = property(get_delta, set_delta,
                      doc='''Gets or sets the delta for the parameter''')
 
+    # Define the property "min_value"
+
     def get_min_value(self):
-        '''Return current minimum allowed value'''
+        """Return current minimum allowed value"""
         return self._min_value
 
     def set_min_value(self, min_value):
-        '''Sets current minimum allowed value'''
+        """Sets current minimum allowed value"""
 
         if min_value is not None and not _behaves_like_a_number(min_value):
-
             raise ValueError("Provided minimum value is not a number nor None")
 
         self._min_value = min_value
 
+        # Check that the current value of the parameter is still within the boundaries. If not, issue a warning
+
+        if self.value < self.min_value:
+            warnings.warn("The current value of the parameter is below the new minimum.", exceptions.RuntimeWarning)
+
     min_value = property(get_min_value, set_min_value,
                          doc='Gets or sets the minimum allowed value for the parameter')
 
+    # Define the property "max_value"
+
     def get_max_value(self):
-        '''Return current minimum allowed value'''
+
+        """Return current minimum allowed value"""
+
         return self._max_value
 
     def set_max_value(self, max_value):
-        '''Sets current minimum allowed value'''
+
+        """Sets current minimum allowed value"""
 
         if max_value is not None and not _behaves_like_a_number(max_value):
-
             raise ValueError("Provided maximum value is not a number nor None")
 
         self._max_value = max_value
+
+        # Check that the current value of the parameter is still within the boundaries. If not, issue a warning
+
+        if self.value > self.max_value:
+            warnings.warn("The current value of the parameter is above the new maximum.", exceptions.RuntimeWarning)
 
     max_value = property(get_max_value, set_max_value,
                          doc='Gets or sets the maximum allowed value for the parameter')
 
     def set_bounds(self, min_value, max_value):
-        '''Sets the boundaries for this parameter to min_value and max_value'''
+        """Sets the boundaries for this parameter to min_value and max_value"""
 
-        #Use the properties so that the checks are made automatically
+        # Use the properties so that the checks are made automatically
 
         self.min_value = min_value
 
         self.max_value = max_value
 
     def get_bounds(self):
-        '''Returns the current boundaries for the parameter'''
+        """Returns the current boundaries for the parameter"""
 
         return self.min_value, self.max_value
 
     def add_callback(self, callback):
-        '''Add a callback to the list of functions which are called whenever the value of the parameter is changed.
+        """Add a callback to the list of functions which are called whenever the value of the parameter is changed.
         The callback must be a function accepting the current value as input. The return value of the callback is
         ignored. More than one callback can be specified. In that case, the callbacks will be called in the same order
-        they have been entered.'''
+        they have been entered."""
 
         self._callbacks.append(callback)
 
     def empty_callbacks(self):
-        '''Remove all callbacks for this parameter'''
-        self._callback = []
+        """Remove all callbacks for this parameter"""
+        self._callbacks = []
+
+    # Define the property "prior"
 
     def get_prior(self):
 
         if self._prior is None:
-
             raise RuntimeError("There is no defined prior for parameter %s" % self.name)
 
         return self._prior
 
     def set_prior(self, prior):
-        '''Set prior for this parameter. The prior must be a function accepting the current value of the parameter
-        as input and giving the probability density as output.'''
+        """Set prior for this parameter. The prior must be a function accepting the current value of the parameter
+        as input and giving the probability density as output."""
 
-        #Try and call the prior with the current value of the parameter
+        # Try and call the prior with the current value of the parameter
         try:
 
-            val = prior(self._value)
+            _ = prior(self._value)
 
         except:
 
             raise RuntimeError("Could not call the provided prior. " +
-                               "Is it a function accepting the currentv value of the parameter?")
+                               "Is it a function accepting the current value of the parameter?")
 
         self._prior = prior
 
-    prior = property(get_prior,set_prior,
-                     doc="Gets or sets the current prior for this parameter. The prior must be a callable function +"
-                         "accepting the current value of the parameter as input and returning the probability "+
+    prior = property(get_prior, set_prior,
+                     doc="Gets or sets the current prior for this parameter. The prior must be a callable function " +
+                         "accepting the current value of the parameter as input and returning the probability " +
                          "density as output")
