f4b1145b9c76dc62dbb409e04fa9429032ce8865
giacomov <giacomo.vianello@gmail.com>
2016-06-08 01:03:46 -0700

    Added the compilation of XSpec

--- setup.py
+++ setup.py
@@ -1,17 +1,117 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+import os
+import sys
 import glob
+import numpy as np
+import re
+
+from setuptools import setup, Extension
+
+
+def setup_xspec():
+
+    headas_root = os.environ.get("HEADAS")
+
+    if headas_root is None:
+
+        print("No HEADAS env. variable set. Xspec support will not be installed ")
+
+        return None
+
+    library_dirs = [os.path.join(headas_root, 'lib')]
+
+    # Check that the library directories exist
+
+    if not os.path.exists(library_dirs[0]):
+
+        print("\nERROR: the library directory %s of HEADAS does not exist!" % library_dirs[0])
+
+        sys.exit(-1)
+
+    libraries = ['XSFunctions', 'XSModel', 'XSUtil', 'XS']
+
+    # Check that the libraries exist
+    for library in libraries:
+
+        library_name = 'lib%s.so' % library
+
+        if not os.path.exists(os.path.join(library_dirs[0], library_name)):
+
+            # See if it has been compiled static (usually libXS is)
+
+            library_name = 'lib%s.a' % library
+
+            if not os.path.exists(os.path.join(library_dirs[0], library_name)):
+
+                print("\nERROR: the library %s does not exist in %s while setting up Xspec!" %
+                      (library, library_dirs[0]))
+
+    # Now find versions for required libraries
+
+    required_libraries = ['cfitsio_', 'CCfits_', 'wcs-']
+
+    for library_to_probe in required_libraries:
+
+        search_path = os.path.join(library_dirs[0], 'lib%s*.so' % library_to_probe)
+
+        print search_path
+
+        versions = glob.glob(search_path)
+
+        if len(versions) == 0:
+            print("\nERROR: cannot find version for library %s while setting up Xspec" % (library_to_probe))
+            sys.exit(-1)
+
+        # Up to there versions[0] is a fully-qualified path
+        # we need instead just the name of the library, without
+        # the lib prefix nor the .so or .a extension
+
+        name = os.path.basename(versions[0])
+        sanitized_name = re.match('lib(.+)\.', name).groups()[0]
+
+        libraries.append(sanitized_name)
+
+    # We also need gfortran
+    libraries.append('gfortran')
+
+    # Configure the variables to build the external module with the C/C++ wrapper
+
+    ext_modules_configuration = [
+
+        Extension("astromodels.xspec._xspec",
+
+                  ["astromodels/xspec/src/_xspec.cc", ],
+
+                  libraries=libraries,
+
+                  include_dirs=['astromodels/xspec/include', np.get_include()],
+
+                  library_dirs=library_dirs,
+                  extra_compile_args=[])]
+
+    return ext_modules_configuration
+
+# Normal packages
+
+packages = ['astromodels',
+            'astromodels/functions',
+            'astromodels/tests',
+            'astromodels/sources',
+            'astromodels/utils'
+            ]
+
+# Check whether we can compile Xspec support
+ext_modules_configuration = setup_xspec()
+
+if ext_modules_configuration is not None:
+
+    packages.append('astromodels/xspec')
 
 setup(
     name="astromodels",
 
-    packages=['astromodels',
-              'astromodels/functions',
-              'astromodels/tests',
-              'astromodels/sources',
-              'astromodels/utils'
-              ],
+    packages=packages,
 
     data_files=[('astromodels/data/functions', glob.glob('astromodels/data/functions/*.yaml'))],
 
@@ -35,6 +135,8 @@ setup(
         'numpy >= 1.6',
         'PyYAML',
         'astropy >= 1.0',
-        'scipy>=0.13']
+        'scipy>=0.13'],
+
+    ext_modules=ext_modules_configuration
 
 )
