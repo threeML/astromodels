450911b74975dcaa6bc9d023b190888ff9638fd4
giacomov <giacomo.vianello@gmail.com>
2016-06-08 01:05:03 -0700

    Importing XSpec models if available

--- astromodels/__init__.py
+++ astromodels/__init__.py
@@ -1,5 +1,3 @@
-__author__ = 'giacomov'
-
 from .sources.point_source import PointSource
 from .sources.extended_source import ExtendedSource
 from .sources.particle_source import ParticleSource
@@ -7,11 +5,25 @@ from parameter import Parameter, IndependentVariable
 from .functions.functions import *
 from .functions.functions_2D import *
 from .functions.functions_3D import *
-from .functions.function import list_functions
+from .functions.function import list_functions, get_function_class
 from model import Model
 from spectral_component import SpectralComponent
 from model_parser import load_model
 from units import get_units
+from .xspec.factory import setup_xspec_models, has_xspec
 import astropy.units as u
 
 astromodels_units = get_units()
+
+# This will either work or issue a warning if XSpec is not available
+
+new_functions = setup_xspec_models()
+
+# Now import the new classes in the local namespace (if any)
+for function_name in new_functions:
+
+    locals()[function_name] = get_function_class(function_name)
+
+if has_xspec:
+
+    from .xspec.xspec_settings import *
