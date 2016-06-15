959ec8333de98075ee60dd2b53865488c1178a38
giacomov <giacomo.vianello@gmail.com>
2016-06-15 13:32:28 -0700

    Added Disk_on_a_sphere function

--- astromodels/functions/functions_2D.py
+++ astromodels/functions/functions_2D.py
@@ -146,3 +146,88 @@ class Gaussian_on_sphere(Function2D):
                 max_lon = max_lon - 360.
 
         return (min_lon, max_lon), (min_lat, max_lat)
+
+
+class Disk_on_sphere(Function2D):
+    r"""
+        description :
+
+            A bidimensional disk/tophat function on a sphere (in spherical coordinates)
+
+        latex : $$ f(\vec{x}) = \left(\frac{180}{\pi}\right)^2 \frac{1}{\pi~({\rm radius})^2} ~\left\{\begin{matrix} 1 & {\rm if}& {\rm | \vec{x} - \vec{x}_0| \le {\rm radius}} \\ 0 & {\rm if}& {\rm | \vec{x} - \vec{x}_0| > {\rm radius}} \end{matrix}\right. $$
+
+        parameters :
+
+            lon0 :
+
+                desc : Longitude of the center of the source
+                initial value : 0.0
+                min : 0.0
+                max : 360.0
+
+            lat0 :
+
+                desc : Latitude of the center of the source
+                initial value : 0.0
+                min : -90.0
+                max : 90.0
+
+            radius :
+
+                desc : Radius of the disk
+                initial value : 0.5
+                min : 0
+                max : 20
+
+        """
+
+    __metaclass__ = FunctionMeta
+
+    def _set_units(self, x_unit, y_unit, z_unit):
+
+        # lon0 and lat0 and rdiff have most probably all units of degrees. However,
+        # let's set them up here just to save for the possibility of using the
+        # formula with other units (although it is probably never going to happen)
+
+        self.lon0.unit = x_unit
+        self.lat0.unit = y_unit
+        self.radius.unit = x_unit
+
+    def evaluate(self, x, y, lon0, lat0, radius):
+
+        lon, lat = x,y
+
+        angsep = angular_distance(lon0, lat0, lon, lat)
+
+        return np.power(180 / np.pi, 2) * 1. / (np.pi * radius ** 2) * (angsep <= radius)
+
+    def get_boundaries(self):
+
+        # Truncate the disk at 2 times the max of radius allowed
+
+        max_radius = self.radius.max_value
+
+        min_lat = max(-90., self.lat0.value - 2 * max_radius)
+        max_lat = min(90., self.lat0.value + 2 * max_radius)
+
+        max_abs_lat = max(np.absolute(min_lat), np.absolute(max_lat))
+
+        if max_abs_lat > 89. or 2 * max_radius / np.cos(max_abs_lat * np.pi / 180.) >= 180.:
+
+            min_lon = 0.
+            max_lon = 360.
+
+        else:
+
+            min_lon = self.lon0.value - 2 * max_radius / np.cos(max_abs_lat * np.pi / 180.)
+            max_lon = self.lon0.value + 2 * max_radius / np.cos(max_abs_lat * np.pi / 180.)
+
+            if min_lon < 0.:
+
+                min_lon = min_lon + 360.
+
+            elif max_lon > 360.:
+
+                max_lon = max_lon - 360.
+
+        return (min_lon, max_lon), (min_lat, max_lat)
