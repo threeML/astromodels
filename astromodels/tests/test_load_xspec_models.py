e232080bee9b4dae1960483ef7f4aec342078b1c
giacomov <giacomo.vianello@gmail.com>
2016-12-27 14:16:44 -0800

    Added test for loading Xspec models

---
+++ astromodels/tests/test_load_xspec_models.py
@@ -0,0 +1,27 @@
+import pytest
+
+try:
+
+    from astromodels.xspec import *
+
+except ImportError:
+
+    has_XSPEC = False
+
+else:
+
+    has_XSPEC = True
+
+from threeML.io.file_utils import sanitize_filename
+
+# This defines a decorator which can be applied to single tests to
+# skip them if the condition is not met
+skip_if_xspec_is_not_available = pytest.mark.skipif(not has_XSPEC,
+                                                    reason="XSPEC not available")
+
+
+@skip_if_xspec_is_not_available
+def test_xspec_load():
+
+    # no need to do anything really
+    pass
