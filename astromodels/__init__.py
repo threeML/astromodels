f17841e1dec90fd7db9ea377703224f0942b4a86
giacomov <giacomo.vianello@gmail.com>
2017-03-07 16:32:59 -0800

    Added a debug mode where names are not imported (to be used in tests)

--- astromodels/__init__.py
+++ astromodels/__init__.py
@@ -3,25 +3,31 @@
 #
 #
 
-from .sources.point_source import PointSource
-from .sources.extended_source import ExtendedSource
-from .sources.particle_source import ParticleSource
-from .core.parameter import Parameter, IndependentVariable, SettingOutOfBounds
-from .functions.functions import *
-from .functions.functions_2D import *
-from .functions.functions_3D import *
-from .functions.template_model import *
-from .functions. dark_matter.dm_models import *
-from .functions.function import list_functions, get_function_class
-from .core.model import Model
-from .core.parameter import Parameter
-from .core.spectral_component import SpectralComponent
-from .core.model_parser import load_model, clone_model
-from .core.units import get_units
-from .core.memoization import use_astromodels_memoization
-import astropy.units as u
+import os
 
-astromodels_units = get_units()
+if os.environ.get("ASTROMODELS_DEBUG", None) is None:
+
+    from .sources.point_source import PointSource
+    from .sources.extended_source import ExtendedSource
+    from .sources.particle_source import ParticleSource
+    from .core.parameter import Parameter, IndependentVariable, SettingOutOfBounds
+    from .functions.functions import *
+    from .functions.functions_2D import *
+    from .functions.functions_3D import *
+    from .functions.template_model import *
+    from .functions. dark_matter.dm_models import *
+    from .functions.function import list_functions, get_function_class
+    from .core.model import Model
+    from .core.parameter import Parameter
+    from .core.spectral_component import SpectralComponent
+    from .core.model_parser import load_model, clone_model
+    from .core.units import get_units
+    from .core.memoization import use_astromodels_memoization
+
+    astromodels_units = get_units()
+
+
+import astropy.units as u
 
 # if has_xspec:
 #
