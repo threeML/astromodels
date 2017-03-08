04841f8e81b8db64e6c1f86ade43d78e2d42fced
giacomov <giacomo.vianello@gmail.com>
2017-03-07 16:38:32 -0800

    Added memory leaks checking

--- astromodels/tests/test_tree.py
+++ astromodels/tests/test_tree.py
@@ -1,8 +1,17 @@
 import pytest
+import os
+
+
+os.environ["ASTROMODELS_DEBUG"] = "debug"
 
 from astromodels.core.tree import Node
 from astromodels.core import node_ctype
 
+import gc
+
+def clean():
+
+    gc.collect()
 
 class _SimpleInheritance(Node):
 
@@ -48,20 +57,22 @@ def test_constructor():
 
         n2 = Node()
 
+    clean()
+
 
 def test_inheritance():
     t = _SimpleInheritance('test1')
 
     assert t.name == 'test1'
 
+    clean()
+
 
 def test_add_child():
 
     t = _SimpleInheritance('test_add_child')
 
-
-
-    with pytest.raises(ValueError):
+    with pytest.raises(TypeError):
         t._add_child("clara")
 
     clara = Node("clara")
@@ -72,7 +83,7 @@ def test_add_child():
 
     # We expect 2 more references: one for the map, one for the vector
 
-    assert node_ctype._get_reference_counts(clara) == clara_ref_count + 3
+    assert node_ctype._get_reference_counts(clara) == clara_ref_count + 1
 
     assert t.clara == clara
 
@@ -81,11 +92,15 @@ def test_add_child():
 
     assert t.clara == clara
 
+    clean()
+
 
 def test_add_children():
     t = _SimpleInheritance("children")
 
     t._add_children([Node("node1"), Node("node2")])
+
+    clean()
 
 
 def test_get_child():
@@ -99,6 +114,8 @@ def test_get_child():
 
     with pytest.raises(AttributeError):
         t._get_child("not existing")
+
+    clean()
 
 
 def test_get_children():
@@ -114,23 +131,29 @@ def test_get_children():
     assert node1._get_children() == (node2, node22)
     assert node2._get_children() == (node3,)
 
+    clean()
+
 
 def test_remove_child():
     t = _SimpleInheritance("test")
 
     n = Node("node")
 
-    t._add_child(n)
+    for i in range(1000):
 
-    assert t._get_child("node") == n
+        t._add_child(n)
 
-    t._remove_child("node")
+        assert t._get_child("node") == n
+
+        t._remove_child("node")
 
     with pytest.raises(AttributeError):
         print t.node
 
     with pytest.raises(AttributeError):
         t._get_child("node")
+
+    clean()
 
 
 def test_get_path():
@@ -147,6 +170,8 @@ def test_get_path():
 
     assert node3.path == "node1.node2.node3"
 
+    clean()
+
 
 def test_get_child_from_path():
     # Make a small tree
@@ -159,6 +184,8 @@ def test_get_child_from_path():
     node2._add_child(node3)
 
     assert node1._get_child_from_path("node2.node3") == node3
+
+    clean()
 
 
 def test_change_name():
@@ -174,6 +201,8 @@ def test_change_name():
     t._change_name("name2")
 
     assert t.name == "name2"
+
+    clean()
 
 
 def test_pickle():
@@ -225,3 +254,189 @@ def test_pickle():
     root2c = pickle.loads(d)
 
     print root2c.min_value
+
+    clean()
+
+
+def test_memory_leaks_setters():
+
+    root = Node("root")
+
+    node = Node("node")
+
+    refc_before_link = node_ctype._get_reference_counts(node)
+
+    root._add_child(node)
+
+    # Adding a node adds 1 reference
+
+    assert node_ctype._get_reference_counts(node) == refc_before_link + 1
+
+    # Remove the node and verify that the reference counts goes back to what it was
+    root._remove_child("node")
+
+    # Now we should go back to the original
+
+    assert node_ctype._get_reference_counts(node) == refc_before_link
+
+    # Now add a second node nested under the first one (root.node.node2)
+    node2 = Node("node2")
+
+    refc_before_link2 = node_ctype._get_reference_counts(node2)
+
+    root._add_child(node)  # +1 for node
+
+    assert node_ctype._get_reference_counts(node) == refc_before_link + 1
+
+    node._add_child(node2)  # +1 for node2 and +1 for node
+
+    assert node_ctype._get_reference_counts(node2) == refc_before_link2 + 1
+
+    # "node" is now also parent of node2, so its reference are now 2 more than the original
+    assert node_ctype._get_reference_counts(node) == refc_before_link + 2
+
+    # Clean up and verify that we go back to the original number of references
+    node._remove_child("node2")  # -1 for node2, -1 for node
+
+    assert node_ctype._get_reference_counts(node2) == refc_before_link2
+
+    assert node_ctype._get_reference_counts(node) == refc_before_link + 1
+
+    root._remove_child("node")  # -1 for node
+
+    assert node_ctype._get_reference_counts(node) == refc_before_link
+
+    # Now test add_children
+    node3 = Node("node3")
+    node4 = Node("node4")
+
+    refc_before_link3 = node_ctype._get_reference_counts(node3)
+    refc_before_link4 = node_ctype._get_reference_counts(node4)
+
+    root._add_children([node3, node4])  # +1 for both nodes
+
+    assert node_ctype._get_reference_counts(node3) == refc_before_link3 + 1
+    assert node_ctype._get_reference_counts(node3) == refc_before_link4 + 1
+
+
+def test_memory_leaks_getters():
+
+    # Now test the getters
+
+    root = Node("root")
+
+    node1 = Node("node1")
+    node2 = Node("node2")
+    node3 = Node("node3")
+
+    refc_before_link_root = node_ctype._get_reference_counts(root)
+    refc_before_link1 = node_ctype._get_reference_counts(node1)
+    refc_before_link2 = node_ctype._get_reference_counts(node2)
+    refc_before_link3 = node_ctype._get_reference_counts(node3)
+
+    None_counts_before = node_ctype._get_reference_counts(None)
+
+    # Add 3 nodes to root
+    root._add_children([node1, node2, node3]) # node1 +1, node2 +1, node3 + 1, root +3
+
+    node_again = root._get_child("node1")  # node1 +1
+
+    assert node_ctype._get_reference_counts(node1) == refc_before_link1 + 2
+
+    del node_again # node1 -1
+
+    assert node_ctype._get_reference_counts(node1) == refc_before_link1 + 1
+
+    children = root._get_children()  #node1 +1, node2 +1, node3 +1
+
+    assert len(children) == 3
+
+    assert node_ctype._get_reference_counts(node1) == refc_before_link1 + 2
+    assert node_ctype._get_reference_counts(node2) == refc_before_link2 + 2
+    assert node_ctype._get_reference_counts(node3) == refc_before_link3 + 2
+    assert node_ctype._get_reference_counts(root) == refc_before_link_root + 3
+
+    # test get_parent
+
+    root_again = node1._get_parent()  # root +1
+
+    assert node_ctype._get_reference_counts(root) == refc_before_link_root + 4
+
+    del root_again  # root -1
+
+    assert node_ctype._get_reference_counts(root) == refc_before_link_root + 3
+
+    # test _get_path
+
+    path = node2._get_path()  # this shouldn't change any ref count
+
+    assert node_ctype._get_reference_counts(node1) == refc_before_link1 + 2
+    assert node_ctype._get_reference_counts(node2) == refc_before_link2 + 2
+    assert node_ctype._get_reference_counts(node3) == refc_before_link3 + 2
+    assert node_ctype._get_reference_counts(root) == refc_before_link_root + 3
+
+    # test _get_child_from_path
+
+    node4 = Node("node4")
+    refc_before_link4 = node_ctype._get_reference_counts(node4)
+
+    node3._add_child(node4)  # node3 +1, node4 + 1
+
+    node4_again = root._get_child_from_path("node3.node4")  # node4 +1
+
+    assert node_ctype._get_reference_counts(node4) == refc_before_link4 + 2
+    assert node_ctype._get_reference_counts(node3) == refc_before_link3 + 3
+
+    del node4_again  # node4 -1
+
+    assert node_ctype._get_reference_counts(node4) == refc_before_link4 + 1
+
+
+def test_memory_leaks_destructors():
+
+    print("\n\n\n\n\n")
+
+    for i in range(1000):
+
+        print("\n\nRound %i" % (i+1))
+
+        # Test destructors
+        root = Node("root")
+
+        node1 = Node("node1")
+        node2 = Node("node2")
+        node3 = Node("node3")
+
+        refc_before_link_root = node_ctype._get_reference_counts(root)
+
+        refc_before_link1 = node_ctype._get_reference_counts(node1)
+        refc_before_link2 = node_ctype._get_reference_counts(node2)
+        refc_before_link3 = node_ctype._get_reference_counts(node3)
+
+        # root._add_children([node1, node2, node3])  # node1 +1, node2 + 1, node3 + 1, root +3
+
+        root._add_child(node1)
+        root._add_child(node2)
+        root._add_child(node3)
+
+
+        assert node_ctype._get_reference_counts(root) == refc_before_link_root + 3
+        assert node_ctype._get_reference_counts(node1) == refc_before_link1 + 1
+        assert node_ctype._get_reference_counts(node2) == refc_before_link2 + 1
+        assert node_ctype._get_reference_counts(node3) == refc_before_link3 + 1
+
+        #
+        # Let's destroy the tree
+        root._remove_child("node1")
+
+        root._remove_child("node2")
+
+        root._remove_child("node3")
+        #
+        # assert node_ctype._get_reference_counts(root) == refc_before_link_root + 3
+        #
+        # # Now deleting root should have decreased all nodes by 3, i..e, should have got them back to the initial
+        # # reference counts
+        # assert node_ctype._get_reference_counts(node1) == refc_before_link1
+        # assert node_ctype._get_reference_counts(node2) == refc_before_link2
+        # assert node_ctype._get_reference_counts(node3) == refc_before_link3
