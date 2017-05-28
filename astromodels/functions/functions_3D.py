a52b76c61b6b9aea319e93a21a02aaec12241218
hayalaso <hayalaso@mtu.edu>
2017-05-28 15:09:26 -0400

    debugging

--- astromodels/functions/functions_3D.py
+++ astromodels/functions/functions_3D.py
@@ -308,7 +308,7 @@ class GalPropTemplate_3D(Function3D):
         lon=l
         lat=b
         #transform energy from keV to MeV. Galprop Model starts at 100 MeV
-        print 1.* u.MeV/u.keV
+        print np.log10(1.* u.MeV/u.keV)
         energy = np.log10(z / 1.e6)
         print energy
         if lon.size != lat.size:
