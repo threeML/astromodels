c1ecd8807807ac83cd310c8edb668d6fa3dcd44f
Hugo Ayala <hgchavo05@gmail.com>
2017-10-13 16:31:11 -0400

    debugging

--- astromodels/functions/functions_3D.py
+++ astromodels/functions/functions_3D.py
@@ -381,6 +381,8 @@ class GalPropTemplate_3D(Function3D):
                 desc : normalization
                 initial value : 1
                 fix : yes
+                max : 10.
+                min : 0.01
 
     """
 
