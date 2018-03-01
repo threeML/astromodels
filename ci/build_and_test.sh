f74ac3c18204ec0f7dfe13bcce2b8ef36360933c
Giacomo Vianello <giacomo.vianello@gmail.com>
2018-02-28 23:40:26 -0800

    Using container

---
+++ ci/build_and_test.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+conda update --yes -q conda
+conda config --set always_yes true
+conda config --set anaconda_upload no
+
+# Create test environment
+conda create --name test_env python=$TRAVIS_PYTHON_VERSION pytest codecov
+
+# Run test
+source activate test_env
+# Build package
+conda build -c conda-forge -c threeml --python=$TRAVIS_PYTHON_VERSION conda-dist/recipe
+# Install it
+conda install --use-local astromodels
+# Run tests
+python -m pytest -vv --cov=astromodels
+
+# Upload coverage measurements
+codecov -t 493c9a2d-42fc-40d6-8e65-24e681efaa1e
+
+# If we are on the master branch upload to the channel
+if [[ "$TRAVIS_BRANCH" == "linux" ]]; then
+        anaconda -t $CONDA_UPLOAD_TOKEN upload -u threeml $CONDA_BLD_PATH/$TRAVIS_OS_NAME-64/*.tar.bz2 --force
+else
+        echo "On a branch, not uploading to Conda channel"
+fi
