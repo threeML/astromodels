bf2ec511939a2ab2b793362b6fbd2565bd76155e
Giacomo Vianello <giacomo.vianello@gmail.com>
2018-04-25 14:35:08 -0700

    Implemented faster angular distance

--- astromodels/utils/angular_distance.py
+++ astromodels/utils/angular_distance.py
@@ -1,6 +1,31 @@
 import numpy as np
 
 
+def angular_distance_fast(ra1, dec1, ra2, dec2):
+    """
+    Compute angular distance using the Haversine formula. Use this one when you know you will never ask for points at
+    their antipodes. If this is not the case, use the angular_distance function which is slower, but works also for
+    antipodes.
+
+    :param lon1:
+    :param lat1:
+    :param lon2:
+    :param lat2:
+    :return:
+    """
+
+    lon1 = np.deg2rad(ra1)
+    lat1 = np.deg2rad(dec1)
+    lon2 = np.deg2rad(ra2)
+    lat2 = np.deg2rad(dec2)
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = np.sin(dlat/2.0)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon /2.0)**2
+    c = 2 * np.arcsin(np.sqrt(a))
+    return np.rad2deg(c)
+
+
 def angular_distance(ra1, dec1, ra2, dec2):
     """
     Returns the angular distance between two points, two sets of points, or a set of points and one point.
@@ -31,6 +56,7 @@ def angular_distance(ra1, dec1, ra2, dec2):
     denominator = slat1 * slat2 + clat1 * clat2 * cdlon
 
     return np.rad2deg(np.arctan2(np.sqrt(num1 ** 2 + num2 ** 2), denominator))
+
 
 def spherical_angle( ra0, dec0, ra1, dec1, ra2, dec2 ):
     """
