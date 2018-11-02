b6835f48f39b0d9cd56e5fe38bdf33cafa776099
Giacomo Vianello <giacomo.vianello@gmail.com>
2018-11-02 11:33:59 -0700

    Fixing linux and mac upload to conda

--- ci/build_and_test.sh
+++ ci/build_and_test.sh
@@ -74,7 +74,17 @@ else
             conda install -c conda-forge anaconda-client
 
             echo "Uploading ${CONDA_BUILD_PATH}"
-
-            anaconda -t $CONDA_UPLOAD_TOKEN upload -u threeml /Users/travis/miniconda/conda-bld/*/*.tar.bz2 --force
+            
+            /opt/conda/conda-bld/linux-64/
+            
+            if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+                
+                anaconda -t $CONDA_UPLOAD_TOKEN upload -u threeml /opt/conda/conda-bld/linux-64/*.tar.bz2 --force
+            
+            else
+            
+                anaconda -t $CONDA_UPLOAD_TOKEN upload -u threeml /Users/travis/miniconda/conda-bld/*/*.tar.bz2 --force
+            
+            fi
         fi
 fi
