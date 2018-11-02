594d6108ee9f79d147fd1813eea9269bd7dd3d40
Giacomo Vianello <giacomo.vianello@gmail.com>
2018-11-02 16:28:34 -0700

    Update factory.py

--- astromodels/xspec/factory.py
+++ astromodels/xspec/factory.py
@@ -14,13 +14,7 @@ from astromodels.utils.configuration import get_user_data_path
 class XSpecNotAvailable(ImportWarning):
     pass
 
-try:
-
-    from astromodels.xspec import _xspec
-
-except ImportError:
-
-    raise XSpecNotAvailable("You need to have XSPEC installed and configured in order to use its models.")
+from astromodels.xspec import _xspec
 
 # When running in a Anaconda environment, the package xspec-modelsonly
 # will install the models data in a specific place, so we set the HEADAS variable to point to the right place for
