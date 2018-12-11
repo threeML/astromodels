Fix Cloning 2Dtemplate model, fix units in some functions (#86)
* Added functionality to clone 2D template functions and write them to yml files

* Fixed parsing of synchrotron function which got broken with the last commit; fixed several functions to work both with and without units

* fix tests

* Fix for linking spatial parameters?

astromodels/functions/functions_2D.py
@@ -274,7 +274,7 @@ class Asymm_Gaussian_on_sphere(Function2D):
         self.lat0.unit = y_unit
         self.a.unit = x_unit
         self.e.unit = u.dimensionless_unscaled
-        self.theta.unit = x_unit
+        self.theta.unit = u.degree
 
     def evaluate(self, x, y, lon0, lat0, a, e, theta):
 
@@ -282,13 +282,23 @@ class Asymm_Gaussian_on_sphere(Function2D):
 
         b = a * np.sqrt(1. - e**2)
         
-        dX = angular_distance( lon0, lat0, lon, lat0);
-        dY = angular_distance( lon0, lat0, lon0, lat);
-        
-        dX=np.where( np.logical_or( np.logical_and( lon-lon0 >0, lon-lon0<180), np.logical_and( lon-lon0<-180, lon-lon0 > -360) ), dX, -dX)
-        dY=np.where( lat>lat0, dY, -dY)
+        dX = np.atleast_1d( angular_distance( lon0, lat0, lon, lat0) )
+        dY = np.atleast_1d( angular_distance( lon0, lat0, lon0, lat) )
 
-        phi = theta + 90.
+        dlon = lon - lon0
+        if isinstance( dlon, u.Quantity):
+            dlon = (dlon.to(u.degree)).value
+        
+        idx=np.logical_and( np.logical_or( dlon < 0, dlon > 180), np.logical_or( dlon>-180, dlon < -360) )
+        dX[idx] = -dX[idx]
+        
+        idx = lat < lat0 
+        dY[idx]=-dY[idx]
+
+        if isinstance( theta, u.Quantity ):
+            phi = (theta.to(u.degree)).value + 90.0
+        else:
+            phi = theta + 90.
 
         cos2_phi = np.power( np.cos( phi * np.pi/180.), 2)
         sin2_phi = np.power( np.sin( phi * np.pi/180.), 2)
@@ -303,7 +313,7 @@ class Asymm_Gaussian_on_sphere(Function2D):
 
         E = -A*np.power(dX, 2) + 2.*B*dX*dY - C*np.power(dY, 2)
 
-        return np.power(180 / np.pi, 2) * 1. / (2 * np.pi * a * b) * np.exp( E )
+        return np.power(180. / np.pi, 2) * 1. / (2 * np.pi * a * b) * np.exp( E )
         
     def get_boundaries(self):
 
@@ -510,13 +520,17 @@ class Ellipse_on_sphere(Function2D):
         self.a.unit = x_unit
         # eccentricity is dimensionless
         self.e.unit = u.dimensionless_unscaled 
-        self.theta.unit = x_unit
+        self.theta.unit = u.degree
         
     def calc_focal_pts(self, lon0, lat0, a, b, theta):
         # focal distance
         f = np.sqrt(a**2 - b**2)
 
-        bearing = 90. - theta
+        if isinstance( theta, u.Quantity):
+          bearing = 90. - (theta.to(u.degree)).value
+        else:
+          bearing = 90. - theta
+          
         # coordinates of focal points
         lon1, lat1 = vincenty(lon0, lat0, bearing, f)
         lon2, lat2 = vincenty(lon0, lat0, bearing + 180., f)
@@ -621,11 +635,20 @@ class SpatialTemplate_2D(Function2D):
     
     def _setup(self):
         
-        self._frame = ICRS()
+        self._frame = "icrs"
+        self._fitsfile = None
+        self._map = None
     
-    def load_file(self,fitsfile,ihdu=0):
+    
+    def load_file(self,fitsfile, ihdu=0):
         
-        with fits.open(fitsfile) as f:
+        if fitsfile is None:
+            
+            raise RuntimeError( "Need to specify a fits file with a template map." )
+        
+        self._fitsfile=fitsfile
+        
+        with fits.open(self._fitsfile) as f:
     
             self._wcs = wcs.WCS( header = f[ihdu].header )
             self._map = f[ihdu].data
@@ -645,6 +668,17 @@ class SpatialTemplate_2D(Function2D):
             h.update( repr(self._wcs) )
             self.hash = int(h.hexdigest(), 16)
             
+
+    def to_dict(self, minimal=False):
+
+         data = super(Function2D, self).to_dict(minimal)
+
+         if not minimal:
+         
+            data['extra_setup'] = {"_fitsfile": self._fitsfile, "_frame": self._frame }
+  
+         return data
+        
     
     def set_frame(self, new_frame):
         """
@@ -653,12 +687,16 @@ class SpatialTemplate_2D(Function2D):
             :param new_frame: a coordinate frame from astropy
             :return: (none)
             """
-        assert isinstance(new_frame, BaseCoordinateFrame)
+        assert new_frame.lower() in ['icrs', 'galactic', 'fk5', 'fk4', 'fk4_no_e' ]
                 
         self._frame = new_frame
     
     def evaluate(self, x, y, K, hash):
         
+        if self._map is None:
+            
+            self.load_file(self._fitsfile)
+          
         # We assume x and y are R.A. and Dec
         coord = SkyCoord(ra=x, dec=y, frame=self._frame, unit="deg")
         
@@ -666,18 +704,23 @@ class SpatialTemplate_2D(Function2D):
         #SkyCoord takes care of necessary coordinate frame transformations.
         Xpix, Ypix = coord.to_pixel(self._wcs)
         
-        Xpix = Xpix.astype(int)
-        Ypix = Ypix.astype(int)
+        Xpix = np.atleast_1d(Xpix.astype(int))
+        Ypix = np.atleast_1d(Ypix.astype(int))
         
         # find pixels that are in the template ROI, otherwise return zero
-        iz = np.where((Xpix<self._nX) & (Xpix>=0) & (Ypix<self._nY) & (Ypix>=0))[0]
-        out = np.zeros((len(x)))
+        #iz = np.where((Xpix<self._nX) & (Xpix>=0) & (Ypix<self._nY) & (Ypix>=0))[0]
+        iz = (Xpix<self._nX) & (Xpix>=0) & (Ypix<self._nY) & (Ypix>=0)
+        out = np.zeros_like(Xpix)
         out[iz] = self._map[Ypix[iz], Xpix[iz]]
         
         return np.multiply(K,out)
 
     def get_boundaries(self):
     
+        if self._map is None:
+            
+            self.load_file(self._fitsfile)
+          
         #We use the max/min RA/Dec of the image corners to define the boundaries.
         #Use the 'outside' of the pixel corners, i.e. from pixel 0 to nX in 0-indexed accounting.
     
@@ -744,6 +787,12 @@ class Power_law_on_sphere(Function2D):
                 initial value : 5.
                 fix : yes
 
+            minr :
+
+                desc : radius below which the PL is approximated as a constant
+                initial value : 0.05
+                fix : yes
+
         """
 
     __metaclass__ = FunctionMeta
@@ -758,21 +807,24 @@ class Power_law_on_sphere(Function2D):
         self.lat0.unit = y_unit
         self.index.unit = u.dimensionless_unscaled
         self.maxr.unit = x_unit
+        self.minr.unit = x_unit
 
-    def evaluate(self, x, y, lon0, lat0, index, maxr):
+    def evaluate(self, x, y, lon0, lat0, index, maxr, minr):
 
         lon, lat = x,y
 
         angsep = angular_distance(lon0, lat0, lon, lat)
 
-        if self.maxr.value <= 0.05:
-            norm = np.power(np.pi / 180., 2.+index) * np.pi * maxr**2 * 0.05**index
+        if maxr <= minr:
+            norm = np.power(np.pi / 180., 2.+index) * np.pi * maxr**2 * minr**index
         elif self.index.value == -2.:
-            norm = np.power(0.05 * np.pi / 180., 2.+index) * np.pi + 2. * np.pi * np.log(maxr / 0.05)
+            norm = np.pi * (1.0 + 2. * np.log(maxr / minr) )
         else:
-            norm = np.power(0.05 * np.pi / 180., 2.+index) * np.pi + 2. * np.pi / (2.+index) * (np.power(maxr * np.pi / 180., index+2.) - np.power(0.05 * np.pi / 180., index+2.))
+            norm = np.power(minr * np.pi / 180., 2.+index) * np.pi + 2. * np.pi / (2.+index) * (np.power(maxr * np.pi / 180., index+2.) - np.power(minr * np.pi / 180., index+2.))
 
-        return np.less_equal(angsep,maxr) * np.power(np.pi / 180., index) * np.power(np.add(np.multiply(angsep, np.greater(angsep, 0.05)), np.multiply(0.05, np.less_equal(angsep, 0.05))), index) / norm
+        value = np.less_equal(angsep,maxr) * np.power(np.pi / 180., index) * np.power(np.add(np.multiply(angsep, np.greater(angsep, minr)), np.multiply(minr, np.less_equal(angsep, minr))), index)
+
+        return value / norm
 
     def get_boundaries(self):
 