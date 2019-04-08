435f8bd968e2335171494d51fe54fa536b39f422
J. Michael Burgess <jmichaelburgess@gmail.com>
2019-04-09 00:07:08 +0200

    trying to follow what sherpa does

--- ci/build_and_test.sh
+++ ci/build_and_test.sh
@@ -14,6 +14,29 @@ cp -R ${TRAVIS_BUILD_DIR}/* my_work_dir/
 
 cd my_work_dir
 
+#### borrowed from conda
+
+# Environment
+libgfortranver="3.0"
+
+xspec_channel=xspec/channel/dev
+
+if [[ ${TRAVIS_OS_NAME} == linux ]];
+then
+    miniconda_os=Linux
+    compilers="gcc_linux-64 gxx_linux-64 gfortran_linux-64"
+else  # osx
+    miniconda_os=MacOSX
+    compilers="clang_osx-64 clangxx_osx-64 gfortran_osx-64"
+
+    # On macOS we also need the conda libx11 libraries used to build xspec
+    # We also need to pin down ncurses, for now only on macos.
+    xorg="xorg-libx11 ncurses=5"
+fi
+
+
+
+
 # Get the version in the __version__ environment variable
 python ci/set_minor_version.py --patch $TRAVIS_BUILD_NUMBER --version_file astromodels/version.py
 
@@ -24,6 +47,23 @@ echo "Building ${PKG_VERSION} ..."
 # Update conda
 conda update --yes -q conda #conda-build
 
+if [[ ${TRAVIS_OS_NAME} == osx ]];
+then
+    conda config --add channels conda-forge
+fi
+
+
+conda config --add channels ${xspec_channel}
+
+# Figure out requested dependencies
+if [ -n "${MATPLOTLIBVER}" ]; then MATPLOTLIB="matplotlib=${MATPLOTLIBVER}"; fi
+if [ -n "${NUMPYVER}" ]; then NUMPY="numpy=${NUMPYVER}"; fi
+if [ -n "${XSPECVER}" ];
+ then export XSPEC="xspec-modelsonly=${XSPECVER} ${xorg}";
+fi
+
+echo "dependencies: ${MATPLOTLIB} ${NUMPY}  ${XSPEC}"
+
 # Answer yes to all questions (non-interactive)
 conda config --set always_yes true
 
@@ -31,7 +71,8 @@ conda config --set always_yes true
 conda config --set anaconda_upload no
 
 # Create test environment
-conda create --name test_env -c conda-forge python=$TRAVIS_PYTHON_VERSION pytest codecov pytest-cov git
+conda create --name test_env -c conda-forge python=$TRAVIS_PYTHON_VERSION pytest codecov pytest-cov git ${MATPLOTLIB} ${NUMPY} ${XSPEC} ${compilers}\
+  libgfortran=${libgfortranver}
 
 # Make sure conda-forge is the first channel
 conda config --add channels conda-forge
