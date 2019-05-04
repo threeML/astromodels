bfda9fb65e38646126ce6e63b9cd9a9875719ec4
J. Michael Burgess <jmichaelburgess@gmail.com>
2019-05-04 12:55:16 +0200

    linux is working, osx has some strange build prefix issue. Trying to disable

--- ci/build_and_test.sh
+++ ci/build_and_test.sh
@@ -51,7 +51,17 @@ if $TEST_WITH_XSPEC ; then
     conda install --use-local -c conda-forge -c threeml astromodels xspec-modelsonly-lite
 else
 
-    conda build -c conda-forge -c threeml --python=$TRAVIS_PYTHON_VERSION conda-dist/no_xspec_recipe
+    if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+	
+	conda build -c conda-forge -c threeml --python=$TRAVIS_PYTHON_VERSION conda-dist/no_xspec_recipe
+
+    else
+
+	# there is some strange error about the prefix length
+
+	conda build --no-prefix-length-fallback -c conda-forge -c threeml --python=$TRAVIS_PYTHON_VERSION conda-dist/no_xspec_recipe
+	
+	
     conda install --use-local -c conda-forge -c threeml astromodels 
 fi
 
