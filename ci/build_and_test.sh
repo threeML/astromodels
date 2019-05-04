58eb49dc6e267357a844eed97b64c51bd1724797
J. Michael Burgess <jmichaelburgess@gmail.com>
2019-05-04 15:40:35 +0200

    update conda build

--- ci/build_and_test.sh
+++ ci/build_and_test.sh
@@ -25,7 +25,7 @@ echo "Testing with XSPEC: ${TEST_WITH_XSPEC} ..."
 
 
 # Update conda
-conda update --yes -q conda #conda-build
+conda update --yes -q conda conda-build
 
 # Answer yes to all questions (non-interactive)
 conda config --set always_yes true
