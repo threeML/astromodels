03913d029f74fd863db05a4326c44bd8a063b09e
J. Michael Burgess <jmichaelburgess@gmail.com>
2019-05-05 14:04:03 +0200

    test

--- ci/build_and_test.sh
+++ ci/build_and_test.sh
@@ -37,7 +37,9 @@ conda config --set anaconda_upload no
 conda create --name test_env -c conda-forge python=$TRAVIS_PYTHON_VERSION pytest codecov pytest-cov git
 
 # Make sure conda-forge is the first channel
-conda config --add channels defaults conda-forge
+conda config --add channels conda-forge
+
+conda config --add channels defaults
 
 # Activate test environment
 source activate test_env
