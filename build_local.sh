39eeb13905bc69a54896d8bcebebaa5dcc51bc3a
omodei <nicola.omodei@gmail.com>
2019-10-04 17:06:07 -0700

    Added a file to compile locally with conda

---
+++ build_local.sh
@@ -0,0 +1,137 @@
+#!/usr/bin/env bash
+
+export TEST_WITH_XSPEC=true
+export TRAVIS_PYTHON_VERSION=3.5
+export TRAVIS_OS_NAME=osx
+export TRAVIS_BUILD_NUMBER=0
+ENVNAME=test_env_$TRAVIS_PYTHON_VERSION
+
+# Make sure we fail in case of errors
+set -e
+
+# Environment
+libgfortranver="3.0"
+XSPECVER="12.10.1b"
+NUMPYVER=1.15
+MATPLOTLIBVER=2
+UPDATE_CONDA=true
+
+xspec_channel=xspec/channel/dev
+
+if [[ ${TRAVIS_OS_NAME} == linux ]];
+then
+    miniconda_os=Linux
+    compilers="gcc_linux-64 gxx_linux-64 gfortran_linux-64"
+else  # osx
+    miniconda_os=MacOSX
+    compilers="clang_osx-64 clangxx_osx-64 gfortran_osx-64"
+
+    # On macOS we also need the conda libx11 libraries used to build xspec
+    # We also need to pin down ncurses, for now only on macos.
+    xorg="xorg-libx11 ncurses=5"
+fi
+
+
+
+
+# Get the version in the __version__ environment variable
+python ci/set_minor_version.py --patch $TRAVIS_BUILD_NUMBER --version_file astromodels/version.py
+
+export PKG_VERSION=$(cd astromodels && python -c "import version;print(version.__version__)")
+
+echo "Building ${PKG_VERSION} ..."
+echo "Python version: ${TRAVIS_PYTHON_VERSION}"
+echo "Testing with XSPEC: ${TEST_WITH_XSPEC} ..."
+
+if $UPDATE_CONDA ; then
+    # Update conda
+    echo "Update conda..."
+    conda update --yes -q conda conda-build
+fi
+
+conda config --add channels ${xspec_channel}
+
+if [[ ${TRAVIS_OS_NAME} == osx ]];
+then
+    conda config --add channels conda-forge
+fi
+
+# Figure out requested dependencies
+if [ -n "${MATPLOTLIBVER}" ]; then MATPLOTLIB="matplotlib=${MATPLOTLIBVER}"; fi
+if [ -n "${NUMPYVER}" ]; then NUMPY="numpy=${NUMPYVER}"; fi
+if [ -n "${XSPECVER}" ];
+ then export XSPEC="xspec-modelsonly=${XSPECVER} ${xorg}";
+fi
+
+echo "dependencies: ${MATPLOTLIB} ${NUMPY}  ${XSPEC}"
+
+# Answer yes to all questions (non-interactive)
+conda config --set always_yes true
+
+# We will upload explicitly at the end, if successful
+conda config --set anaconda_upload no
+
+# Create test environment
+echo "Create test environment..."
+conda create --yes --name $ENVNAME -c conda-forge python=$TRAVIS_PYTHON_VERSION pytest codecov pytest-cov git ${MATPLOTLIB} ${NUMPY} ${XSPEC} astropy ${compilers}\
+  libgfortran=${libgfortranver}
+
+# Make sure conda-forge is the first channel
+conda config --add channels conda-forge
+
+conda config --add channels defaults
+
+# Activate test environment
+echo "Activate test environment..."
+
+source activate $ENVNAME
+
+# Build package
+echo "Build package..."
+if $TEST_WITH_XSPEC ; then
+    echo "Building WITH xspec"
+    if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+        conda build -c conda-forge -c threeml --python=$TRAVIS_PYTHON_VERSION conda-dist/recipe
+        conda index /opt/conda/conda-bld
+    else
+    	# there is some strange error about the prefix length
+        conda build --no-build-id --python=$TRAVIS_PYTHON_VERSION conda-dist/recipe
+        conda index $HOME/miniconda/conda-bld
+    fi
+	echo "======> installing..."
+    conda install --use-local -c conda-forge -c threeml astromodels
+    # xspec-modelsonly
+else
+
+    if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+
+	    conda build -c conda-forge -c threeml --python=$TRAVIS_PYTHON_VERSION conda-dist/no_xspec_recipe
+        conda index /opt/conda/conda-bld
+
+    else
+
+	# there is some strange error about the prefix length
+
+	    conda build --no-build-id  --python=$TRAVIS_PYTHON_VERSION conda-dist/no_xspec_recipe
+        conda index $HOME/miniconda/conda-bld
+
+    fi
+
+	echo "======>  installing..."
+    conda install --use-local -c conda-forge -c threeml astromodels
+fi
+
+
+# Run tests
+cd astromodels/tests
+python -m pytest -vv --cov=astromodels # -k "not slow"
+
+# Codecov needs to run in the main git repo
+
+# Upload coverage measurements if we are on Linux
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+
+    echo "********************************** COVERAGE ******************************"
+    codecov -t 493c9a2d-42fc-40d6-8e65-24e681efaa1e
+
+fi
