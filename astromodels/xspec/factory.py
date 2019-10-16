7743957e2366f6a802e9896d3283a420496a27ae
omodei <nicola.omodei@gmail.com>
2019-10-16 15:09:57 -0700

    Check type in hardmax and hardmin

--- astromodels/xspec/factory.py
+++ astromodels/xspec/factory.py
@@ -341,18 +341,19 @@ def get_models(model_dat_path):
 
                 raise ValueError("Illegal identifier name %s" % (par_name))
 
-            if hard_maximum < hard_minimum:
 
-                raise ValueError("Hard maximum (%s) < hard minimum (%s)" %(hard_maximum,hard_minimum))
+            if hard_maximum is not None and hard_minimum is not None:
 
-            if float(default_value) > hard_maximum:
+                if (float(hard_maximum) < float(hard_minimum)):
 
-                if hard_maximum is not None:
+                    raise ValueError("Hard maximum (%s) < hard minimum (%s)" %(hard_maximum,hard_minimum))
+
+                if float(default_value) > float(hard_maximum):
+
                     default_value = hard_maximum
 
-            elif float(default_value) < hard_minimum:
+                elif float(default_value) < float(hard_minimum):
 
-                if hard_minimum is not None:
                     default_value = hard_minimum
 
 
