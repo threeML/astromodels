f08ccb12535204723993a9679943e59489183a7b
grburgess <jmichaelburgess@gmail.com>
2021-01-14 12:23:07 +0100

    trying to update the node

--- astromodels/core/tree.py
+++ astromodels/core/tree.py
@@ -1,13 +1,19 @@
-from future import standard_library
-standard_library.install_aliases()
-from builtins import object
 import collections
+from builtins import object
+from dataclasses import dataclass, field
+from typing import Dict, List, Optional
+
+from future import standard_library
+
+from astromodels.utils.io import display
+from astromodels.utils.valid_variable import is_valid_variable_name
 
 from .cpickle_compatibility_layer import cPickle
 
-from astromodels.utils.io import display
-from astromodels.core.node_ctype import _Node
-from astromodels.utils.valid_variable import is_valid_variable_name
+standard_library.install_aliases()
+
+
+#from astromodels.core.node_ctype import _Node
 
 
 class DuplicatedNode(Exception):
@@ -32,6 +38,103 @@ class NewNodeUnpickler(object):
 
         return instance
 
+@dataclass(repr=False)
+class _Node:
+    _name: str
+    _parent: Optional[_Node] = field(repr=False, default=None)
+    _children: Dict[str, _Node] = field(default_factory=dict, repr=False)
+    _path: Optional[str] = field(repr=False, default="")
+
+    def _add_child(self, child: _Node):
+
+        if child._name not in self._children:
+            self._children[child._name] = child
+            child._set_parent(self)
+
+        else:
+
+            raise RuntimeError(
+                f"A child with name {child._name} already exists")
+
+    def _add_children(self, children: List[_Node]):
+
+        for child in children:
+            self._add_child(child)
+
+    def _remove_child(self, name: str):
+        del self._children[name]
+        # return self._children.pop(name)
+
+    def _set_parent(self, parent: _Node):
+
+        self._parent = parent
+        self._path = f"{self._parent._get_path()}.{self._name}"
+
+    def _get_child(self, name: str):
+
+        return self._children[name]
+
+    def _has_child(self, name: str):
+        return name in self._children
+
+    def _get_children(self):
+        return tuple(self._children.values())
+
+    def _get_child_from_path(self, path: str):
+
+        nodes = path.split('.')
+        _next = self
+        for node in nodes:
+            _next = _next._get_child(node)
+
+        return _next
+
+    def _get_parent(self):
+        return self._parent
+
+    def _get_path(self):
+
+        if self._parent is not None:
+            return self._path
+
+        else:
+            return self._name
+
+    @property
+    def path(self):
+        return self._get_path()
+
+    def _update_path(self):
+
+        # recursively update the path
+
+        for name, child in self._children.items():
+
+            child._path = f"{child._parent._get_path()}.{child._name}"
+            child._update_path()
+
+    def _change_name(self, name: str):
+
+        self._name = name
+        self._parent._children.pop(self._name)
+        self._set_parent(self)
+
+        # update all the children
+        self._update_path()
+
+    @property
+    def name(self):
+        return self._name
+
+    def __dir__(self):
+        return super().__dir__() + list(self._children.keys())
+
+    def __getattr__(self, name):
+        if name in self._children:
+            return self._children[name]
+        else:
+            return super().__getattr__(name)
+
 
 class Node(_Node):
 
@@ -46,27 +149,39 @@ class Node(_Node):
 
         _Node.__init__(self, name)
 
-    #########################################################################
+        #########################################################################
     # The next 3 methods are *really* necessary for anything to work
 
     def __reduce__(self):
 
         state = {}
+        state['parent'] = self._get_parent()
+        state['path'] = self._path
         state['children'] = self._get_children()
-        state['name'] = self.name
+        state['name'] = self._name
         state['__dict__'] = self.__dict__
 
         return NewNodeUnpickler(), (self.__class__,), state
 
     def __setstate__(self, state):
 
-        # Set the children
-
-        self._add_children(state['children'])
+        self._children = {}
 
         # Set the name of this node
 
-        self._change_name(state['name'])
+        self._name = state['name']
+
+        # Set the parent
+
+        self._parent = state['parent']
+
+        # set the path
+
+        self._path = state['path']
+
+        # Set the children
+
+        self._add_children(state['children'])
 
         # Restore everything else
 
@@ -79,18 +194,7 @@ class Node(_Node):
 
         return cPickle.loads(cPickle.dumps(self))
 
-    #########################################################################
-
     # This is used by dir() and by the autocompletion in Ipython
-    def __dir__(self):
-
-        # Get the names of the attributes of the class
-        l = list(self.__class__.__dict__.keys())
-
-        # Get all the children
-        l.extend([child.name for child in self._get_children()])
-
-        return l
 
     def to_dict(self, minimal=False):
 
@@ -134,200 +238,3 @@ class Node(_Node):
         # This will automatically choose the best representation among repr and repr_html
 
         display(self)
-
-
-class OldNode(object):
-
-    def __init__(self, name):
-
-        self._children = collections.OrderedDict()
-        self._parent = None
-
-        assert is_valid_variable_name(name), "Illegal characters in name %s. You can only use letters and numbers, " \
-                                             "and _" % name
-
-        self._name = name
-
-    @property
-    def name(self):
-        """
-        Returns the name of the node
-
-        :return: a string containing the name
-        """
-        return self._name
-
-    @property
-    def path(self):
-
-        return ".".join(self._get_path())
-
-    def _add_children(self, children):
-
-        for child in children:
-
-            self._add_child(child)
-
-    def _add_child(self, new_child, name=None, add_attribute=True):
-
-        new_child._set_parent(self)
-
-        if name is None:
-
-            name = new_child.name
-
-        if name in self._children:
-
-            raise DuplicatedNode("You cannot use the same name (%s) for different nodes" % name)
-
-        self._children[name] = new_child
-
-        if add_attribute:
-
-            # Add also an attribute with the name of the new child, to allow access with a syntax like
-            # node.child
-
-            object.__setattr__(self, name, new_child)
-
-    def _get_child(self, child_name):
-
-        return self._children[child_name]
-
-    def _remove_child(self, child_name):
-
-        object.__delattr__(self, child_name)
-
-        return self._children.pop(child_name)
-
-    def _set_parent(self, parent):
-
-        # We need to use directly the __setattr__ method because the normal self._children = ... will trigger
-        # an exception, because of the __setattr__ method of the DualAccessClass which forbids changing
-        # nodes. However, this might reassign the parent to a class
-
-        object.__setattr__(self, "_parent", parent)
-
-    def _get_parent(self):
-
-        return self._parent
-
-    def _get_child_from_path(self, path):
-        """
-        Return a children below this level, starting from a path of the kind "this_level.something.something.name"
-
-        :param path: the key
-        :return: the child
-        """
-
-        keys = path.split(".")
-
-        this_child = self
-
-        for key in keys:
-
-            try:
-
-                this_child = this_child._get_child(key)
-
-            except KeyError:
-
-                raise KeyError("Child %s not found" % path)
-
-        return this_child
-
-    def _get_path(self):
-
-        parent_names = []
-
-        current_node = self
-
-        while True:
-
-            this_parent = current_node._get_parent()
-
-            if this_parent is None:
-
-                break
-
-            else:
-
-                parent_names.append(current_node.name)
-
-                current_node = this_parent
-
-        return parent_names[::-1]
-
-    def to_dict(self, minimal=False):
-
-        this_dict = collections.OrderedDict()
-
-        for key, val in list(self._children.items()):
-
-            this_dict[key] = val.to_dict(minimal)
-
-        return this_dict
-
-    def _repr__base(self, rich_output):
-
-        raise NotImplementedError("You should implement the __repr__base method for each class")
-
-    def __repr__(self):
-        """
-        Textual representation for console
-
-        :return: representation
-        """
-
-        return self._repr__base(rich_output=False)
-
-    def _repr_html_(self):
-        """
-        HTML representation for the IPython notebook
-
-        :return: HTML representation
-        """
-
-        return self._repr__base(rich_output=True)
-
-    def display(self):
-        """
-        Display information about the point source.
-
-        :return: (none)
-        """
-
-        # This will automatically choose the best representation among repr and repr_html
-
-        display(self)
-
-    def _find_instances(self, cls):
-        """
-        Find all the instances of cls below this node.
-
-        :return: a dictionary of instances of cls
-        """
-
-        instances = collections.OrderedDict()
-
-        for child_name, child in list(self._children.items()):
-
-            if isinstance(child, cls):
-
-                key_name = ".".join(child._get_path())
-
-                instances[key_name] = child
-
-                # Now check if the instance has children,
-                # and if it does go deeper in the tree
-
-                # NOTE: an empty dictionary evaluate as False
-
-                if child._children:
-
-                    instances.update(child._find_instances(cls))
-
-            else:
-
-                instances.update(child._find_instances(cls))
-
-        return instances
--- astromodels/tests/test_tree.py
+++ astromodels/tests/test_tree.py
@@ -1,21 +1,23 @@
 from __future__ import print_function
-from builtins import range
-import pytest
-import os
 
+import gc
+import os
+from builtins import range
+
+import astropy.units as u
+import pytest
+
+from astromodels.core.tree import Node
 
 os.environ["ASTROMODELS_DEBUG"] = "debug"
 
-from astromodels.core.tree import Node
-from astromodels.core import node_ctype
+#from astromodels.core import node_ctype
 
-import astropy.units as u
-
-import gc
 
 def clean():
 
     gc.collect()
+
 
 class _SimpleInheritance(Node):
 
@@ -81,13 +83,13 @@ def test_add_child():
 
     clara = Node("clara")
 
-    clara_ref_count = node_ctype._get_reference_counts(clara)
+#    clara_ref_count = node_ctype._get_reference_counts(clara)
 
     t._add_child(clara)
 
     # We expect 2 more references: one for the map, one for the vector
 
-    assert node_ctype._get_reference_counts(clara) == clara_ref_count + 1
+#    assert node_ctype._get_reference_counts(clara) == clara_ref_count + 1
 
     assert t.clara == clara
 
@@ -262,185 +264,185 @@ def test_pickle():
     clean()
 
 
-def test_memory_leaks_setters():
+# def test_memory_leaks_setters():
 
-    root = Node("root")
+#     root = Node("root")
 
-    node = Node("node")
+#     node = Node("node")
 
-    refc_before_link = node_ctype._get_reference_counts(node)
+#     refc_before_link = node_ctype._get_reference_counts(node)
 
-    root._add_child(node)
+#     root._add_child(node)
 
-    # Adding a node adds 1 reference
+#     # Adding a node adds 1 reference
 
-    assert node_ctype._get_reference_counts(node) == refc_before_link + 1
+#     assert node_ctype._get_reference_counts(node) == refc_before_link + 1
 
-    # Remove the node and verify that the reference counts goes back to what it was
-    root._remove_child("node")
+#     # Remove the node and verify that the reference counts goes back to what it was
+#     root._remove_child("node")
 
-    # Now we should go back to the original
+#     # Now we should go back to the original
 
-    assert node_ctype._get_reference_counts(node) == refc_before_link
+#     assert node_ctype._get_reference_counts(node) == refc_before_link
 
-    # Now add a second node nested under the first one (root.node.node2)
-    node2 = Node("node2")
+#     # Now add a second node nested under the first one (root.node.node2)
+#     node2 = Node("node2")
 
-    refc_before_link2 = node_ctype._get_reference_counts(node2)
+#     refc_before_link2 = node_ctype._get_reference_counts(node2)
 
-    root._add_child(node)  # +1 for node
+#     root._add_child(node)  # +1 for node
 
-    assert node_ctype._get_reference_counts(node) == refc_before_link + 1
+#     assert node_ctype._get_reference_counts(node) == refc_before_link + 1
 
-    node._add_child(node2)  # +1 for node2 and +1 for node
+#     node._add_child(node2)  # +1 for node2 and +1 for node
 
-    assert node_ctype._get_reference_counts(node2) == refc_before_link2 + 1
+#     assert node_ctype._get_reference_counts(node2) == refc_before_link2 + 1
 
-    # "node" is now also parent of node2, so its reference are now 2 more than the original
-    assert node_ctype._get_reference_counts(node) == refc_before_link + 2
+#     # "node" is now also parent of node2, so its reference are now 2 more than the original
+#     assert node_ctype._get_reference_counts(node) == refc_before_link + 2
 
-    # Clean up and verify that we go back to the original number of references
-    node._remove_child("node2")  # -1 for node2, -1 for node
+#     # Clean up and verify that we go back to the original number of references
+#     node._remove_child("node2")  # -1 for node2, -1 for node
 
-    assert node_ctype._get_reference_counts(node2) == refc_before_link2
+#     assert node_ctype._get_reference_counts(node2) == refc_before_link2
 
-    assert node_ctype._get_reference_counts(node) == refc_before_link + 1
+#     assert node_ctype._get_reference_counts(node) == refc_before_link + 1
 
-    root._remove_child("node")  # -1 for node
+#     root._remove_child("node")  # -1 for node
 
-    assert node_ctype._get_reference_counts(node) == refc_before_link
+#     assert node_ctype._get_reference_counts(node) == refc_before_link
 
-    # Now test add_children
-    node3 = Node("node3")
-    node4 = Node("node4")
+#     # Now test add_children
+#     node3 = Node("node3")
+#     node4 = Node("node4")
 
-    refc_before_link3 = node_ctype._get_reference_counts(node3)
-    refc_before_link4 = node_ctype._get_reference_counts(node4)
+#     refc_before_link3 = node_ctype._get_reference_counts(node3)
+#     refc_before_link4 = node_ctype._get_reference_counts(node4)
 
-    root._add_children([node3, node4])  # +1 for both nodes
+#     root._add_children([node3, node4])  # +1 for both nodes
 
-    assert node_ctype._get_reference_counts(node3) == refc_before_link3 + 1
-    assert node_ctype._get_reference_counts(node3) == refc_before_link4 + 1
+#     assert node_ctype._get_reference_counts(node3) == refc_before_link3 + 1
+#     assert node_ctype._get_reference_counts(node3) == refc_before_link4 + 1
 
 
-def test_memory_leaks_getters():
+# def test_memory_leaks_getters():
 
-    # Now test the getters
+#     # Now test the getters
 
-    root = Node("root")
+#     root = Node("root")
 
-    node1 = Node("node1")
-    node2 = Node("node2")
-    node3 = Node("node3")
+#     node1 = Node("node1")
+#     node2 = Node("node2")
+#     node3 = Node("node3")
 
-    refc_before_link_root = node_ctype._get_reference_counts(root)
-    refc_before_link1 = node_ctype._get_reference_counts(node1)
-    refc_before_link2 = node_ctype._get_reference_counts(node2)
-    refc_before_link3 = node_ctype._get_reference_counts(node3)
+#     refc_before_link_root = node_ctype._get_reference_counts(root)
+#     refc_before_link1 = node_ctype._get_reference_counts(node1)
+#     refc_before_link2 = node_ctype._get_reference_counts(node2)
+#     refc_before_link3 = node_ctype._get_reference_counts(node3)
 
-    None_counts_before = node_ctype._get_reference_counts(None)
+#     None_counts_before = node_ctype._get_reference_counts(None)
 
-    # Add 3 nodes to root
-    root._add_children([node1, node2, node3]) # node1 +1, node2 +1, node3 + 1, root +3
+#     # Add 3 nodes to root
+#     root._add_children([node1, node2, node3]) # node1 +1, node2 +1, node3 + 1, root +3
 
-    node_again = root._get_child("node1")  # node1 +1
+#     node_again = root._get_child("node1")  # node1 +1
 
-    assert node_ctype._get_reference_counts(node1) == refc_before_link1 + 2
+#     assert node_ctype._get_reference_counts(node1) == refc_before_link1 + 2
 
-    del node_again # node1 -1
+#     del node_again # node1 -1
 
-    assert node_ctype._get_reference_counts(node1) == refc_before_link1 + 1
+#     assert node_ctype._get_reference_counts(node1) == refc_before_link1 + 1
 
-    children = root._get_children()  #node1 +1, node2 +1, node3 +1
+#     children = root._get_children()  #node1 +1, node2 +1, node3 +1
 
-    assert len(children) == 3
+#     assert len(children) == 3
 
-    assert node_ctype._get_reference_counts(node1) == refc_before_link1 + 2
-    assert node_ctype._get_reference_counts(node2) == refc_before_link2 + 2
-    assert node_ctype._get_reference_counts(node3) == refc_before_link3 + 2
-    assert node_ctype._get_reference_counts(root) == refc_before_link_root + 3
+#     assert node_ctype._get_reference_counts(node1) == refc_before_link1 + 2
+#     assert node_ctype._get_reference_counts(node2) == refc_before_link2 + 2
+#     assert node_ctype._get_reference_counts(node3) == refc_before_link3 + 2
+#     assert node_ctype._get_reference_counts(root) == refc_before_link_root + 3
 
-    # test get_parent
+#     # test get_parent
 
-    root_again = node1._get_parent()  # root +1
+#     root_again = node1._get_parent()  # root +1
 
-    assert node_ctype._get_reference_counts(root) == refc_before_link_root + 4
+#     assert node_ctype._get_reference_counts(root) == refc_before_link_root + 4
 
-    del root_again  # root -1
+#     del root_again  # root -1
 
-    assert node_ctype._get_reference_counts(root) == refc_before_link_root + 3
+#     assert node_ctype._get_reference_counts(root) == refc_before_link_root + 3
 
-    # test _get_path
+#     # test _get_path
 
-    path = node2._get_path()  # this shouldn't change any ref count
+#     path = node2._get_path()  # this shouldn't change any ref count
 
-    assert node_ctype._get_reference_counts(node1) == refc_before_link1 + 2
-    assert node_ctype._get_reference_counts(node2) == refc_before_link2 + 2
-    assert node_ctype._get_reference_counts(node3) == refc_before_link3 + 2
-    assert node_ctype._get_reference_counts(root) == refc_before_link_root + 3
+#     assert node_ctype._get_reference_counts(node1) == refc_before_link1 + 2
+#     assert node_ctype._get_reference_counts(node2) == refc_before_link2 + 2
+#     assert node_ctype._get_reference_counts(node3) == refc_before_link3 + 2
+#     assert node_ctype._get_reference_counts(root) == refc_before_link_root + 3
 
-    # test _get_child_from_path
+#     # test _get_child_from_path
 
-    node4 = Node("node4")
-    refc_before_link4 = node_ctype._get_reference_counts(node4)
+#     node4 = Node("node4")
+#     refc_before_link4 = node_ctype._get_reference_counts(node4)
 
-    node3._add_child(node4)  # node3 +1, node4 + 1
+#     node3._add_child(node4)  # node3 +1, node4 + 1
 
-    node4_again = root._get_child_from_path("node3.node4")  # node4 +1
+#     node4_again = root._get_child_from_path("node3.node4")  # node4 +1
 
-    assert node_ctype._get_reference_counts(node4) == refc_before_link4 + 2
-    assert node_ctype._get_reference_counts(node3) == refc_before_link3 + 3
+#     assert node_ctype._get_reference_counts(node4) == refc_before_link4 + 2
+#     assert node_ctype._get_reference_counts(node3) == refc_before_link3 + 3
 
-    del node4_again  # node4 -1
+#     del node4_again  # node4 -1
 
-    assert node_ctype._get_reference_counts(node4) == refc_before_link4 + 1
+#     assert node_ctype._get_reference_counts(node4) == refc_before_link4 + 1
 
 
-def test_memory_leaks_destructors():
+# def test_memory_leaks_destructors():
 
-    print("\n\n\n\n\n")
+#     print("\n\n\n\n\n")
 
-    for i in range(1000):
+#     for i in range(1000):
 
-        print("\n\nRound %i" % (i+1))
+#         print("\n\nRound %i" % (i+1))
 
-        # Test destructors
-        root = Node("root")
+#         # Test destructors
+#         root = Node("root")
 
-        node1 = Node("node1")
-        node2 = Node("node2")
-        node3 = Node("node3")
+#         node1 = Node("node1")
+#         node2 = Node("node2")
+#         node3 = Node("node3")
 
-        refc_before_link_root = node_ctype._get_reference_counts(root)
+#         refc_before_link_root = node_ctype._get_reference_counts(root)
 
-        refc_before_link1 = node_ctype._get_reference_counts(node1)
-        refc_before_link2 = node_ctype._get_reference_counts(node2)
-        refc_before_link3 = node_ctype._get_reference_counts(node3)
+#         refc_before_link1 = node_ctype._get_reference_counts(node1)
+#         refc_before_link2 = node_ctype._get_reference_counts(node2)
+#         refc_before_link3 = node_ctype._get_reference_counts(node3)
 
-        # root._add_children([node1, node2, node3])  # node1 +1, node2 + 1, node3 + 1, root +3
+#         # root._add_children([node1, node2, node3])  # node1 +1, node2 + 1, node3 + 1, root +3
 
-        root._add_child(node1)
-        root._add_child(node2)
-        root._add_child(node3)
+#         root._add_child(node1)
+#         root._add_child(node2)
+#         root._add_child(node3)
 
 
-        assert node_ctype._get_reference_counts(root) == refc_before_link_root + 3
-        assert node_ctype._get_reference_counts(node1) == refc_before_link1 + 1
-        assert node_ctype._get_reference_counts(node2) == refc_before_link2 + 1
-        assert node_ctype._get_reference_counts(node3) == refc_before_link3 + 1
+#         assert node_ctype._get_reference_counts(root) == refc_before_link_root + 3
+#         assert node_ctype._get_reference_counts(node1) == refc_before_link1 + 1
+#         assert node_ctype._get_reference_counts(node2) == refc_before_link2 + 1
+#         assert node_ctype._get_reference_counts(node3) == refc_before_link3 + 1
 
-        #
-        # Let's destroy the tree
-        root._remove_child("node1")
+#         #
+#         # Let's destroy the tree
+#         root._remove_child("node1")
 
-        root._remove_child("node2")
+#         root._remove_child("node2")
 
-        root._remove_child("node3")
-        #
-        # assert node_ctype._get_reference_counts(root) == refc_before_link_root + 3
-        #
-        # # Now deleting root should have decreased all nodes by 3, i..e, should have got them back to the initial
-        # # reference counts
-        # assert node_ctype._get_reference_counts(node1) == refc_before_link1
-        # assert node_ctype._get_reference_counts(node2) == refc_before_link2
-        # assert node_ctype._get_reference_counts(node3) == refc_before_link3
+#         root._remove_child("node3")
+#         #
+#         # assert node_ctype._get_reference_counts(root) == refc_before_link_root + 3
+#         #
+#         # # Now deleting root should have decreased all nodes by 3, i..e, should have got them back to the initial
+#         # # reference counts
+#         # assert node_ctype._get_reference_counts(node1) == refc_before_link1
+#         # assert node_ctype._get_reference_counts(node2) == refc_before_link2
+#         # assert node_ctype._get_reference_counts(node3) == refc_before_link3
