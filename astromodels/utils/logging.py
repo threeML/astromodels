220d14578b1cccc08c319a100f1d81e718b9a59d
J. Michael Burgess <jmichaelburgess@gmail.com>
2021-01-29 18:46:04 +0100

    Improve logging (#143)
    * added our own colored logg
    
    * update logger
    
    * rm uneeded imports

--- astromodels/utils/logging.py
+++ astromodels/utils/logging.py
@@ -7,11 +7,10 @@
 import logging
 import logging.handlers as handlers
 import sys
+from hashlib import sha256
 from pathlib import Path
-from typing import Dict, Optional
 
-import colorama
-from colorama import Back, Fore, Style
+from .sys_tools import colored, supports_color
 
 try:
 
@@ -22,11 +21,6 @@ try:
 except:
 
     has_threeml = False
-
-
-colorama.deinit()
-colorama.init(strip=False)
-# set up the console logging
 
 
 def get_path_of_log_dir():
@@ -62,27 +56,169 @@ def get_path_of_log_file(log_file: str) -> Path:
     return get_path_of_log_dir() / log_file
 
 
+DEFAULT_LOG_COLORS = {
+    'DEBUG': 'thin_blue',
+    'INFO': 'green',
+    'WARNING': 'purple',
+    'ERROR': 'red',
+    'CRITICAL': 'bold_red',
+
+}
+
+
+def esc(*x):
+    """Create escaped code from format code"""
+    return '\033[' + ';'.join(x) + 'm'
+
+
+# The following coloured log logic is from
+# https://github.com/borntyping/python-colorlog
+# I dropped some features and removed the Python 2.7 compatibility
+
+ESCAPE_CODES = {'reset': esc('0'), 'bold': esc('01'), 'thin': esc('02')}
+
+COLORS = ['black', 'red', 'green', 'yellow', 'blue', 'purple', 'cyan', 'white']
+
+PREFIXES = [
+    # Foreground without prefix
+    ('3', ''),
+    ('01;3', 'bold_'),
+    ('02;3', 'thin_'),
+
+    # Foreground with fg_ prefix
+    ('3', 'fg_'),
+    ('01;3', 'fg_bold_'),
+    ('02;3', 'fg_thin_'),
+
+    # Background with bg_ prefix - bold/light works differently
+    ('4', 'bg_'),
+    ('10', 'bg_bold_'),
+]
+
+for _prefix, _prefix_name in PREFIXES:
+    for _code, _name in enumerate(COLORS):
+        ESCAPE_CODES[_prefix_name + _name] = esc(_prefix + str(_code))
+
+
+def parse_colors(sequence):
+    """Return escape codes from a color sequence."""
+    return ''.join(ESCAPE_CODES[n] for n in sequence.split(',') if n)
+
+
+class ColoredRecord:
+    """
+    Wraps a LogRecord, adding named escape codes to the internal dict.
+    The internal dict is used when formatting the message (by the PercentStyle,
+    StrFormatStyle, and StringTemplateStyle classes).
+    """
+
+    def __init__(self, record):
+        """Add attributes from the escape_codes dict and the record."""
+        self.__dict__.update(ESCAPE_CODES)
+        self.__dict__.update(record.__dict__)
+        self.__record = record
+
+    def __getattr__(self, name):
+        return getattr(self.__record, name)
+
+
 class ColoredFormatter(logging.Formatter):
     """
-    Colored log formatter.
+    A formatter that allows colors to be placed in the format string.
+    Intended to help in creating more readable logging output.
+    Based on https://github.com/borntyping/python-colorlog
     """
 
-    def __init__(
-        self, *args, colors: Optional[Dict[str, str]] = None, **kwargs
-    ) -> None:
-        """Initialize the formatter with specified format strings."""
+    def __init__(self,
+                 fmt,
+                 datefmt=None,
+                 style='%',
+                 log_colors=None,
+                 reset=True,
+                 secondary_log_colors=None):
+        """
+        Set the format and colors the ColouredFormatter will use.
+        The ``fmt``, ``datefmt`` and ``style`` args are passed on to the
+        ``logging.Formatter`` constructor.
+        The ``secondary_log_colors`` argument can be used to create additional
+        ``log_color`` attributes. Each key in the dictionary will set
+        ``{key}_log_color``, using the value to select from a different
+        ``log_colors`` set.
+        :Parameters:
+        - fmt (str): The format string to use
+        - datefmt (str): A format string for the date
+        - log_colors (dict):
+            A mapping of log level names to color names
+        - reset (bool):
+            Implicitly append a color reset to all records unless False
+        - style ('%' or '{' or '$'):
+            The format style to use. (*No meaning prior to Python 3.2.*)
+        - secondary_log_colors (dict):
+            Map secondary ``log_color`` attributes. (*New in version 2.6.*)
+        """
+        super(ColoredFormatter, self).__init__(fmt, datefmt, style)
 
-        super().__init__(*args, **kwargs)
+        self.log_colors = (log_colors
+                           if log_colors is not None else DEFAULT_LOG_COLORS)
+        self.secondary_log_colors = secondary_log_colors
+        self.reset = reset
 
-        self.colors = colors if colors else {}
+    def format(self, record):
+        """Format a message from a record object."""
+        record = ColoredRecord(record)
+        record.log_color = escape_codes(self.log_colors, record.levelname)
 
-    def format(self, record) -> str:
-        """Format the specified record as text."""
+        if self.secondary_log_colors:
+            for name, log_colors in self.secondary_log_colors.items():
+                color = escape_codes(log_colors, record.levelname)
+                setattr(record, name + '_log_color', color)
 
-        record.color = self.colors.get(record.levelname, "")
-        record.reset = Style.RESET_ALL
+        message = super(ColoredFormatter, self).format(record)
 
-        return super().format(record)
+        if self.reset and not message.endswith(ESCAPE_CODES['reset']):
+            message += ESCAPE_CODES['reset']
+
+        return message
+
+
+def escape_codes(log_colors, level_name):
+    """Return escape codes from a ``log_colors`` dict."""
+    return parse_colors(log_colors.get(level_name, ""))
+
+
+def hash_coloured(text):
+    """Return a ANSI coloured text based on its hash"""
+    ansi_code = int(sha256(text.encode('utf-8')).hexdigest(), 16) % 230
+    return colored(text, ansi_code=ansi_code)
+
+
+def hash_coloured_escapes(text):
+    """Return the ANSI hash colour prefix and suffix for a given text"""
+    ansi_code = int(sha256(text.encode('utf-8')).hexdigest(), 16) % 230
+    prefix, suffix = colored('SPLIT', ansi_code=ansi_code).split('SPLIT')
+    return prefix, suffix
+
+# class ColoredFormatter(logging.Formatter):
+#     """
+#     Colored log formatter.
+#     """
+
+#     def __init__(
+#         self, *args, colors: Optional[Dict[str, str]] = None, **kwargs
+#     ) -> None:
+#         """Initialize the formatter with specified format strings."""
+
+#         super().__init__(*args, **kwargs)
+
+#         self.colors = colors if colors else {}
+
+#     def format(self, record) -> str:
+#         """Format the specified record as text."""
+
+#         record.color = self.colors.get(record.levelname, "")
+#         record.reset = Style.RESET_ALL
+
+#         return super().format(record)
 
 
 class LogFilter(object):
@@ -124,19 +260,20 @@ _usr_formatter = logging.Formatter(
 astromodels_usr_log_handler.setFormatter(_usr_formatter)
 
 # now set up the console logger
+name = "test"
+if supports_color():
+    prefix_1, suffix = hash_coloured_escapes(name)
+    prefix_2, _ = hash_coloured_escapes(name + 'salt')
+else:
+    prefix_1, prefix_2, suffix = ('', '', '')
 
-_console_formatter = ColoredFormatter(
-    "{color} {levelname:8} {reset}| {color} {message} {reset}",
-    style="{",
-    datefmt="%H:%M:%S",
-    colors={
-        "DEBUG": Fore.CYAN,
-        "INFO": Fore.GREEN + Style.BRIGHT,
-        "WARNING": Fore.YELLOW + Style.DIM,
-        "ERROR": Fore.RED + Style.BRIGHT,
-        "CRITICAL": Fore.RED + Back.WHITE + Style.BRIGHT,
-    },
-)
+date_str = ''
+
+_console_formatter = ColoredFormatter('%(log_color)s[%(levelname)-8s]'
+                                      '%(log_color)s %(message)s',
+                                      datefmt="%H:%M:%S",
+
+                                      )
 
 astromodels_console_log_handler = logging.StreamHandler(sys.stdout)
 astromodels_console_log_handler.setFormatter(_console_formatter)
