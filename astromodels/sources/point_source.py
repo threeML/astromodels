dc3bf8499ded253950f64bf0247a6404d84b0e81
grburgess <jmichaelburgess@gmail.com>
2021-06-06 10:26:39 +0200

    clean up

--- astromodels/sources/point_source.py
+++ astromodels/sources/point_source.py
@@ -1,26 +1,24 @@
 from __future__ import division
-from astromodels.core.parameter import Parameter
-from astromodels.functions.function import Function1D
-from past.utils import old_div
-import collections
 
-from typing import Dict, List, Optional, Union, Any
+import collections
+from typing import Any, Dict, List, Optional, Union
 
 import astropy.units as u
-import numpy
 import numba as nb
+import numpy
 import scipy.integrate
+from past.utils import old_div
 
+from astromodels.core.memoization import use_astromodels_memoization
+from astromodels.core.parameter import Parameter
 from astromodels.core.sky_direction import SkyDirection
 from astromodels.core.spectral_component import SpectralComponent
 from astromodels.core.tree import Node
 from astromodels.core.units import get_units
-from astromodels.sources.source import Source, POINT_SOURCE
-from astromodels.utils.pretty_list import dict_to_list
-from astromodels.core.memoization import use_astromodels_memoization
+from astromodels.functions.function import Function1D
+from astromodels.sources.source import POINT_SOURCE, Source
 from astromodels.utils.logging import setup_logger
-
-
+from astromodels.utils.pretty_list import dict_to_list
 
 __author__ = 'giacomov'
 
