3e8d5f488e49b1e56ee837e995cebca317d99315
grburgess <jmichaelburgess@gmail.com>
2021-08-17 12:53:00 +0200

    rm table models?

--- astromodels/xspec/src/_xspec.cc
+++ astromodels/xspec/src/_xspec.cc
@@ -329,12 +329,12 @@ void tdrellinelpext_(float* ear, int* ne, float* param, int* ifl, float* photar,
 // XSPEC table models; in XSPEC 12.10.1 these have been consolidated
 // into the tabint routine.
 //
-#ifndef XSPEC_12_10_1
-void xsatbl(float* ear, int ne, float* param, const char* filenm, int ifl,
-	    float* photar, float* photer);
-void xsmtbl(float* ear, int ne, float* param, const char* filenm, int ifl,
-	    float* photar, float* photer);
-#endif
+// #ifndef XSPEC_12_10_1
+// void xsatbl(float* ear, int ne, float* param, const char* filenm, int ifl,
+// 	    float* photar, float* photer);
+// void xsmtbl(float* ear, int ne, float* param, const char* filenm, int ifl,
+// 	    float* photar, float* photer);
+// #endif
 
 // XSPEC convolution models
 //
