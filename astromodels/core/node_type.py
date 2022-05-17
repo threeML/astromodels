1fd6352882c468c2afff33ef236e9d63827a4c10
grburgess <jmichaelburgess@gmail.com>
2022-05-17 12:01:46 -0400

    save name of children in state to prevent recursion

--- astromodels/core/node_type.py
+++ astromodels/core/node_type.py
@@ -24,9 +24,9 @@ class NewNodeUnpickler(object):
 class NodeBase:
     _name: str
     _parent: Optional[Type["NodeBase"]] = field(repr=False, default=None)
-    _children: Dict[str, Type["NodeBase"]] = field(default_factory=dict,
-                                                   repr=False,
-                                                   compare=False)
+    _children: Dict[str, Type["NodeBase"]] = field(
+        default_factory=dict, repr=False, compare=False
+    )
     _path: Optional[str] = field(repr=False, default="")
 
     # The next 3 methods are *really* necessary for anything to work
@@ -34,13 +34,14 @@ class NodeBase:
     def __reduce__(self):
 
         state = {}
-        state['parent'] = self._get_parent()
-        state['path'] = self._path
-        state['children'] = self._get_children()
-        state['name'] = self._name
-        state['__dict__'] = self.__dict__
+        state["parent"] = self._get_parent()
+        state["path"] = self._path
+        state["children"] = self._get_children()
+        state["child_names"] = [child.name for child in state["children"]]
+        state["name"] = self._name
+        state["__dict__"] = self.__dict__
 
-        return NewNodeUnpickler(), (self.__class__, ), state
+        return NewNodeUnpickler(), (self.__class__,), state
 
     def __setstate__(self, state) -> None:
 
@@ -48,25 +49,29 @@ class NodeBase:
 
         # Set the name of this node
 
-        self._name = state['name']
+        self._name = state["name"]
 
         # Set the parent
 
-        self._parent = state['parent']
+        self._parent = state["parent"]
 
         # set the path
 
-        self._path = state['path']
+        self._path = state["path"]
 
         # Set the children
 
-        self._add_children(state['children'])
+        for child, name in zip(state["children"], state["child_names"]):
+
+            self._children[name] = child
+
+        # self._add_children(state['children'])
 
         # Restore everything else
 
-        for k in state['__dict__']:
+        for k in state["__dict__"]:
 
-            self.__dict__[k] = state['__dict__'][k]
+            self.__dict__[k] = state["__dict__"][k]
 
     # This is necessary for copy.deepcopy to work
     def __deepcopy__(self, memodict={}):
@@ -110,9 +115,7 @@ class NodeBase:
         for child in children:
             self._add_child(child)
 
-    def _remove_child(self,
-                      name: str,
-                      delete: bool = True) -> Optional["NodeBase"]:
+    def _remove_child(self, name: str, delete: bool = True) -> Optional["NodeBase"]:
         """
         return a child
         """
@@ -138,9 +141,9 @@ class NodeBase:
 
             # we want to get the
             # orphan back
-            
+
             return child
-            
+
             # return self._children.pop(name)
 
     def _orphan(self) -> None:
@@ -150,14 +153,13 @@ class NodeBase:
         """
 
         # disconnect from parent
-        
+
         self._parent = None
 
         # be nice to the kids and tell them
-        
+
         self._update_child_path()
-        
-        
+
     def _set_parent(self, parent: Type["NodeBase"]) -> None:
         """
         set the parent and update path
@@ -168,16 +170,15 @@ class NodeBase:
         parent_path = self._parent._get_path()
 
         if parent_path == "__root__":
-        
+
             self._path = f"{self._name}"
 
         else:
 
             self._path = f"{parent_path}.{self._name}"
-            
+
         log.debug(f"path is now: {self._path}")
 
-        
     def _get_child(self, name: str) -> "NodeBase":
         """
         return a child object
@@ -202,7 +203,7 @@ class NodeBase:
         """
         get a child from a string path
         """
-        nodes = path.split('.')
+        nodes = path.split(".")
         _next = self
         for node in nodes:
             _next = _next._get_child(node)
@@ -218,19 +219,21 @@ class NodeBase:
 
         for child in node._get_children():
 
-            #log.debug(f"on child {child._name}")
-            
+            # log.debug(f"on child {child._name}")
+
             if isinstance(child, node_type):
 
                 path = child._get_path()
 
-                #log.debug(f"on child {path}")
-                
+                # log.debug(f"on child {path}")
+
                 instances[path] = child
 
                 for sub_child in child._get_children():
 
-                    instances.update(self._recursively_gather_node_type(sub_child, node_type))
+                    instances.update(
+                        self._recursively_gather_node_type(sub_child, node_type)
+                    )
 
             else:
 
@@ -238,8 +241,6 @@ class NodeBase:
 
         return instances
 
-
-    
     def _get_parent(self) -> "NodeBase":
         return self._parent
 
@@ -253,7 +254,7 @@ class NodeBase:
         else:
             return self._name
 
-    def _root(self, source_only: bool=False) -> "NodeBase":
+    def _root(self, source_only: bool = False) -> "NodeBase":
         """
         returns the root of the node, will stop at the source
         if source_only is set to True
@@ -267,11 +268,11 @@ class NodeBase:
 
             # recursively walk up the tree to
             # the root
-            
+
             while True:
 
                 parent = current_node._parent
-                
+
                 if source_only:
 
                     if parent.name == "__root__":
@@ -283,7 +284,7 @@ class NodeBase:
                 if current_node.is_root:
 
                     return current_node
-        
+
     @property
     def path(self) -> str:
         return self._get_path()
@@ -293,8 +294,8 @@ class NodeBase:
 
         Update the path of all children recursively.
         This is needed if the name is changed
-        
-        :returns: 
+
+        :returns:
 
         """
         # recursively update the path
@@ -304,10 +305,9 @@ class NodeBase:
             child._path = f"{child._parent._get_path()}.{child._name}"
 
             if not child.is_leaf:
-            
+
                 child._update_child_path()
-        
-            
+
     def _change_name(self, name: str, clear_parent: bool = False) -> None:
         """
         change the name of this node. This will have to update the
@@ -318,7 +318,7 @@ class NodeBase:
 
         if (self._parent is not None) and (not clear_parent):
 
-#            self._parent._children.pop(self._name)
+            #            self._parent._children.pop(self._name)
             self._set_parent(self._parent)
 
         # update all the children
@@ -346,43 +346,47 @@ class NodeBase:
     @property
     def name(self) -> str:
         return self._name
-  
+
     def __getattr__(self, name):
+
+        print(name)
+
         if name in self._children:
 
             return self._children[name]
 
         else:
 
-            #log.error(f"Accessing an element {name} of the node that does not exist")
-            
-            raise AttributeError(f"Accessing an element {name} of the node that does not exist")
+            # log.error(f"Accessing an element {name} of the node that does not exist")
 
-            
-            #return super(NodeBase).__getattr__(name)
+            raise AttributeError(
+                f"Accessing an element {name} of the node that does not exist"
+            )
+
+            # return super(NodeBase).__getattr__(name)
 
     def __setattr__(self, name, value):
 
         ### We cannot change a node
         ### but if the node has a value
-        ### attribute, we want to call that 
-        
-        
+        ### attribute, we want to call that
+
         if "_children" in self.__dict__:
             if name in self._children:
 
-                if ("_internal_value" in self._children[name].__dict__):
+                if "_internal_value" in self._children[name].__dict__:
 
                     if not self._children[name].is_leaf:
 
-                        log.warning(f"Trying to set the value of a linked parameter ({name}) directly has no effect ")
+                        log.warning(
+                            f"Trying to set the value of a linked parameter ({name}) directly has no effect "
+                        )
 
-                        
                         return
-                        
+
                     else:
                         # ok, this is likely  parameter
-                    
+
                         self._children[name].value = value
 
                 else:
@@ -392,7 +396,9 @@ class NodeBase:
 
                     # log.error(f"Accessing an element {name} of the node that does not exist")
 
-                    raise AttributeError(f"Accessing an element {name} of the node that does not exist")
+                    raise AttributeError(
+                        f"Accessing an element {name} of the node that does not exist"
+                    )
             else:
                 return super().__setattr__(name, value)
         else:
@@ -401,7 +407,7 @@ class NodeBase:
 
     def plot_tree(self) -> None:
         """
-        this plots the tree to the 
+        this plots the tree to the
         screen
         """
 
@@ -412,13 +418,13 @@ class NodeBase:
         if self.is_leaf:
             return self.name
 
-        child_strs = [child._plot_tree_console()
-                      for child in list(self._children.values())]
+        child_strs = [
+            child._plot_tree_console() for child in list(self._children.values())
+        ]
         child_widths = [block_width(s) for s in child_strs]
 
         # How wide is this block?
-        display_width = max(len(self.name),
-                            sum(child_widths) + len(child_widths) - 1)
+        display_width = max(len(self.name), sum(child_widths) + len(child_widths) - 1)
 
         # Determines midpoints of child blocks
         child_midpoints = []
@@ -431,23 +437,22 @@ class NodeBase:
         brace_builder = []
         for i in range(display_width):
             if i < child_midpoints[0] or i > child_midpoints[-1]:
-                brace_builder.append(' ')
+                brace_builder.append(" ")
             elif i in child_midpoints:
-                brace_builder.append('+')
+                brace_builder.append("+")
             else:
-                brace_builder.append('-')
-        brace = ''.join(brace_builder)
+                brace_builder.append("-")
+        brace = "".join(brace_builder)
 
-        name_str = '{:^{}}'.format(self.name, display_width)
+        name_str = "{:^{}}".format(self.name, display_width)
         below = stack_str_blocks(child_strs)
 
-        return name_str + '\n' + brace + '\n' + below
+        return name_str + "\n" + brace + "\n" + below
 
 
-    
 def block_width(block) -> int:
     try:
-        return block.index('\n')
+        return block.index("\n")
     except ValueError:
         return len(block)
 
@@ -466,16 +471,16 @@ def stack_str_blocks(blocks) -> str:
     """
     builder = []
     block_lens = [block_width(bl) for bl in blocks]
-    split_blocks = [bl.split('\n') for bl in blocks]
+    split_blocks = [bl.split("\n") for bl in blocks]
 
     for line_list in itertools.zip_longest(*split_blocks, fillvalue=None):
         for i, line in enumerate(line_list):
             if line is None:
-                builder.append(' ' * block_lens[i])
+                builder.append(" " * block_lens[i])
             else:
                 builder.append(line)
             if i != len(line_list) - 1:
-                builder.append(' ')  # Padding
-        builder.append('\n')
+                builder.append(" ")  # Padding
+        builder.append("\n")
 
-    return ''.join(builder[:-1])
+    return "".join(builder[:-1])
