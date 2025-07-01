a2b1acc5163ba106f742f7b07835365f6979a8ee
GallegoSav <123730578+GallegoSav@users.noreply.github.com>
2025-07-01 12:41:06 +0200

    Update functions_2D.py

--- astromodels/functions/functions_2D.py
+++ astromodels/functions/functions_2D.py
@@ -780,7 +780,7 @@ class SpatialTemplate_2D(Function2D, metaclass=FunctionMeta):
 
     def evaluate(self, x, y, K, hash, ihdu):
 
-        # X and Y definition are defined by the frame (ICRS,galactic, etc..)
+        # X and Y are defined by the frame (ICRS,galactic, etc..)
         coord = SkyCoord(x, y, frame=self.frame.value, unit="deg")
 
         # transform input coordinates to pixel coordinates;
