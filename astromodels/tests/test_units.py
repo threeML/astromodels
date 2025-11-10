d934b6b3714f3ddc52f6dda218071b6ff90ce3d3
Tobias Preis <tobias.preis@uibk.ac.at>
2025-11-10 07:39:46 +0100

    test units

--- astromodels/tests/test_units.py
+++ astromodels/tests/test_units.py
@@ -1,9 +1,12 @@
+from copy import deepcopy
 import astropy.units as u
 from omegaconf import OmegaConf
 
-from astromodels.core.units import get_units
+from astromodels.core.units import get_units, set_units
 from astromodels.utils.configuration import astromodels_config
 from astromodels.utils.file_utils import get_path_of_user_config
+from astromodels.functions.functions_1D.powerlaws import Powerlaw
+from astromodels.sources.point_source import PointSource
 
 
 def test_config_unit_same():
@@ -15,3 +18,22 @@ def test_config_unit_same():
                 assert astromodels_config["units"][k] == user_conf["units"][k]
     for k, v in astromodels_config["units"].items():
         assert u.Unit(v) == getattr(get_units(), k)
+
+
+def test_set_units():
+    mapping = {"energy": u.TeV, "area": u.m**2, "time": u.h, "angle": u.rad}
+    ref = deepcopy(get_units())
+    not_changed = []
+    for k, v in mapping.items():
+        if getattr(ref, k) != v:
+            set_units(k, v)
+        else:
+            not_changed.append(k)
+    for k, v in mapping.items():
+        if k not in not_changed:
+            assert getattr(get_units(), k) == v
+            assert getattr(ref, k) != getattr(get_units(), k)
+
+    # reset it to the original
+    for k, v in mapping.items():
+        set_units(k, getattr(ref, k))
