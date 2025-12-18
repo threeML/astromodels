10a7b48869dfbcc919106b1783e6ee7c0ef732ca
Tobias Preis <tobias.preis@uibk.ac.at>
2025-12-18 16:43:24 +0100

    increase test coverage on xspec factory

--- astromodels/tests/test_load_xspec_models.py
+++ astromodels/tests/test_load_xspec_models.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import os
+import collections
 
 import astropy.units as u
 import pytest
@@ -8,6 +10,10 @@ from astromodels import Model, PointSource, clone_model, load_model
 try:
 
     from astromodels.xspec import XS_bbody, XS_phabs, XS_powerlaw
+    from astromodels.xspec.factory import (
+        find_model_dat,
+        get_models,
+    )
 
 except (ImportError, ModuleNotFoundError):
 
@@ -55,3 +61,28 @@ def test_xspec_saving():
     p = Path(filename)
 
     p.unlink()
+
+
+@skip_if_xspec_is_not_available
+def test_find_model_dat():
+    model_dat_path = find_model_dat()
+    assert Path(model_dat_path).is_file(), "model.dat file path is not a file"
+
+    headas_env = os.environ.get("HEADAS")
+    os.environ.update({"HEADAS": "/"})  # set HEADAS to root dir
+    with pytest.raises(FileNotFoundError):
+        find_model_dat()
+    os.environ.update({"HEADAS": headas_env})
+
+
+@skip_if_xspec_is_not_available
+def test_get_models():
+    model_definitions = get_models(find_model_dat())
+    assert isinstance(model_definitions, collections.OrderedDict)
+
+    # the gaussian line should be available in all versions supported
+    assert (
+        "agauss",
+        "C_agauss",
+        "add",
+    ) in model_definitions.keys(), "agauss not in model.dat"
