98632171bd1dff7f6b34fb585b7db569d799e7da
Tobias Preis <tobias.preis@uibk.ac.at>
2026-01-07 11:13:13 +0100

    add test for jitted pl and cpl evaluation#

--- astromodels/tests/test_numba_functions.py
+++ astromodels/tests/test_numba_functions.py
@@ -1,4 +1,3 @@
-import pytest
 from astromodels.functions.numba_functions import (
     _expm1,
     _exp,
@@ -9,16 +8,6 @@ from astromodels.functions.numba_functions import (
     plaw_eval,
     plaw_flux_norm,
     cplaw_eval,
-    cplaw_inverse_eval,
-    super_cplaw_eval,
-    band_eval,
-    bplaw_eval,
-    sbplaw_eval,
-    bb_eval,
-    mbb_eval,
-    ggrb_int_pl,
-    non_diss_photoshere_generic,
-    dbl_sbpl,
 )
 import numpy as np
 
@@ -30,3 +19,14 @@ def test_vectorized_functions():
     assert (_pow(np.array([2, 2, 2]), 2) == np.array([4, 4, 4])).all
     assert (_log(np.array([1, 1, 1])) == np.array([0, 0, 0])).all
     assert (_log10(np.array([1, 1, 1])) == np.array([0, 0, 0])).all
+
+
+def test_jitted_functions():
+    x = np.array([0.1, 1, 10, 100])
+    assert (plaw_eval.py_func(x, 1, -2, 1) == np.array([100, 1, 0.01, 1e-4])).all
+    assert np.isclose(plaw_flux_norm.py_func(-2.0, 1, 10), 2.302585)
+    assert np.isclose(plaw_flux_norm.py_func(-4.0, 1, 10), 0.495)
+    assert np.isclose(
+        cplaw_eval.py_func(x, 1, 100, -2, 1),
+        np.array([99.90005, 0.99005, 9.095 * 1e-3, 3.6788 * 1e-5]),
+    ).all
