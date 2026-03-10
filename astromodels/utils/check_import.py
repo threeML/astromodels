49d6c1cf83673f437a76424ef39e9a61fbf13d49
Tobias Preis <tobias.preis@uibk.ac.at>
2026-03-10 18:16:57 +0100

    add: check import function

---
+++ astromodels/utils/check_import.py
@@ -0,0 +1,9 @@
+from importlib.util import find_spec
+
+
+def check_import(module_name, class_name="Function"):
+    if find_spec(module_name) is None:
+        raise RuntimeError(
+            f"{module_name} is required for {class_name} that you are trying to use."
+            "Install it and try again."
+        )
