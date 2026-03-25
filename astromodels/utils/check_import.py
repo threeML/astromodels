7e55a734a5369c730f231dabc3f42a9dc6d8429a
Tobias Preis <tobias.preis@uibk.ac.at>
2026-03-25 10:23:30 +0100

    fix: warn instead fail

--- astromodels/utils/check_import.py
+++ astromodels/utils/check_import.py
@@ -1,11 +1,13 @@
 from importlib.util import find_spec
+import warnings
 
 
 def check_import(module_name, class_name="Function"):
     if find_spec(module_name) is None:
-        raise RuntimeError(
+        warnings.warn(
             f"{module_name} is required for {class_name} that you are trying to use."
             "Install it and try again."
         )
+        return False
     else:
         return True
