21d008da54140042dafefe2360f94a49f941b8cb
Tobias Preis <tobias.preis@uibk.ac.at>
2026-03-27 07:16:59 +0100

    test: also cover serialization

--- astromodels/tests/test_model.py
+++ astromodels/tests/test_model.py
@@ -29,6 +29,7 @@ from astromodels.functions.functions_1D.functions import _ComplexTestFunction
 from astromodels.sources.extended_source import ExtendedSource
 from astromodels.sources.particle_source import ParticleSource
 from astromodels.sources.point_source import PointSource
+import astromodels.core.serialization  # noqa: F401
 
 
 def _get_point_source(name="test"):
