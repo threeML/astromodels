f40dffda674b16842e9e7d89fbad11f0281fd374
Tobias Preis <tobias.preis@uibk.ac.at>
2026-03-31 14:29:04 +0200

    fix: flake8

--- astromodels/tests/test_extended_source.py
+++ astromodels/tests/test_extended_source.py
@@ -158,7 +158,8 @@ def test_call():
         if name == "SpatialTemplate_2D_Healpix":
             if not has_mhealpy:
                 pytest.skip(
-                    "Skipping SpatialTemplate_2D_Healpix test because mhealpy is missing"
+                    "Skipping SpatialTemplate_2D_Healpix test because "
+                    "mhealpy is missing"
                 )
             make_test_healpix_template("__test.fits")
             shape = class_type(fits_file="__test.fits")
@@ -256,7 +257,8 @@ def test_call_with_units():
         if name == "SpatialTemplate_2D_Healpix":
             if not has_mhealpy:
                 pytest.skip(
-                    "Skipping SpatialTemplate_2D_Healpix test because mhealpy is missing"
+                    "Skipping SpatialTemplate_2D_Healpix test because "
+                    "mhealpy is missing"
                 )
             make_test_healpix_template("__test.fits")
 
