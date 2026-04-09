e7db6cc0851d9fc4f6236bab8e43384decfe7f7b
Tobias Preis <tobias.preis@uibk.ac.at>
2026-04-09 14:10:53 +0200

    tests: temporary tests for integral error assignment

--- astromodels/tests/test_integration.py
+++ astromodels/tests/test_integration.py
@@ -6,6 +6,7 @@ def test_analytical_integral():
     pl = Powerlaw()
     pl.index.value = -2
     assert np.isclose(pl.integrate(0.1, 1), 9)
+    assert pl.integral_error is None
 
 
 def test_numerical_integral():
@@ -13,3 +14,4 @@ def test_numerical_integral():
     line.a.value = 0
     line.b.value = 1
     assert np.isclose(line.integrate(0, 1), 0.5)
+    assert line.integral_error is not None
